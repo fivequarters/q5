9da63e4aedf943ff359c81146d0d02148e0ac5a2
Randall Tombaugh <randall@fivequarters.io>
2019-02-24 08:21:03 -1000

    added google site-verification meta-tag

--- site/corp/webpack.prod.js
+++ site/corp/webpack.prod.js
@@ -4,7 +4,10 @@ const options = {
   hash: true,
   html: {
     title: 'Five Quarters',
-    meta: [{ name: 'description', content: "Five Quarters. We're making SaaS integrations just work better." }],
+    meta: [
+      { name: 'description', content: "Five Quarters. We're making SaaS integrations just work better." },
+      { name: 'google-site-verification', content: 'Xsqrjgp5Y9GZRzH9jGGwqQmI0tP-jSVRfUYXX5l4EGc' },
+    ],
   },
 };
 
