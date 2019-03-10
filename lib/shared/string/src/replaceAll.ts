612570935d7c488db294b9e965af19a9c62c16e8
Randall Tombaugh <randall@fivequarters.io>
2019-03-10 13:43:15 -1000

    add replaceAll to string package

--- lib/shared/string/src/replaceAll.ts
+++ lib/shared/string/src/replaceAll.ts
@@ -3,16 +3,6 @@
 // ------------------
 
 export function replaceAll(text: string, search: string, replacement: string) {
-  let nextIndex = 0;
-  while (true) {
-    console.log('DEBUG:', nextIndex);
-    nextIndex = text.indexOf(search, nextIndex);
-    console.log('DEBUG:', nextIndex, JSON.stringify(text));
-    if (nextIndex === -1) {
-      return text;
-    }
-    console.log('DEBUG:', replacement.length);
-    text = text.replace(search, replacement);
-    nextIndex += replacement.length; //+ (replacement.indexOf('\u001b') >= 0 ? 1 : 0);
-  }
+  const split = text.split(search);
+  return split.join(replacement);
 }
