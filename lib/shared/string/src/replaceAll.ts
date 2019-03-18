0d04b5edc3a45ae536e944cd7f8427c24e8e1b87
Randall Tombaugh <randall@fivequarters.io>
2019-03-17 20:17:17 -1000

    remove the string package

--- lib/shared/string/src/replaceAll.ts
+++
@@ -1,8 +0,0 @@
-// ------------------
-// Exported Functions
-// ------------------
-
-export function replaceAll(text: string, search: string, replacement: string) {
-  const split = text.split(search);
-  return split.join(replacement);
-}
