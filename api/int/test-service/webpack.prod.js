7a3ed0b257be77d56dfac30f405a1c687dac3247
Randall Tombaugh <randall@fivequarters.io>
2019-03-26 14:40:03 -1000

    clean up

--- api/int/test-service/webpack.prod.js
+++
@@ -1,9 +0,0 @@
-const { webpackDev } = require('@5qtrs/tool-config');
-const packageJson = require('./package.json');
-const options = {
-  entry: 'lambda',
-  targetNode: true,
-  libraryTarget: 'umd',
-};
-
-module.exports = webpackDev(packageJson, options);
