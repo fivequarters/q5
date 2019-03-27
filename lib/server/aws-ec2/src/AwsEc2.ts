faa0baad27c859e32a51f0eae11d430e32063241
Tomasz Janczuk <tomasz@janczuk.org>
2019-03-27 13:34:22 -0700

    export flexd-mono ec2 logs and metrics to cloud watch

--- lib/server/aws-ec2/src/AwsEc2.ts
+++ lib/server/aws-ec2/src/AwsEc2.ts
@@ -56,9 +56,93 @@ apt-get update
 apt-get install -y docker-ce
 apt-get install -y awscli
 
+# Get docker image of Flexd
+
 $(aws ecr get-login --region ${region} --no-include-email)
 
 docker pull ${account}.dkr.ecr.${region}.amazonaws.com/${repo}:${tag}
+
+# Install and configure AWS Unified Cloud Watch Agent
+
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+dpkg -i -E ./amazon-cloudwatch-agent.deb
+
+cat > /opt/aws/amazon-cloudwatch-agent/bin/config.json << EOF
+{
+	"logs": {
+		"logs_collected": {
+			"files": {
+				"collect_list": [
+					{
+						"file_path": "/var/log/syslog",
+						"log_group_name": "/flexd-mono/${deploymentName}",
+						"log_stream_name": "{instance_id}"
+					}
+				]
+			}
+		}
+	},
+	"metrics": {
+		"append_dimensions": {
+      "FlexdDeploymentName": "${deploymentName}",
+			"AutoScalingGroupName": "\\\${aws:AutoScalingGroupName}",
+			"ImageId": "\\\${aws:ImageId}",
+			"InstanceId": "\\\${aws:InstanceId}",
+			"InstanceType": "\\\${aws:InstanceType}"
+		},
+		"metrics_collected": {
+			"cpu": {
+				"measurement": [
+					"cpu_usage_idle",
+					"cpu_usage_iowait",
+					"cpu_usage_user",
+					"cpu_usage_system"
+				],
+				"metrics_collection_interval": 60,
+				"totalcpu": false
+			},
+			"disk": {
+				"measurement": [
+					"used_percent",
+					"inodes_free"
+				],
+				"metrics_collection_interval": 60,
+				"resources": [
+					"*"
+				]
+			},
+			"diskio": {
+				"measurement": [
+					"io_time"
+				],
+				"metrics_collection_interval": 60,
+				"resources": [
+					"*"
+				]
+			},
+			"mem": {
+				"measurement": [
+					"mem_used_percent"
+				],
+				"metrics_collection_interval": 60
+			},
+			"swap": {
+				"measurement": [
+					"swap_used_percent"
+				],
+				"metrics_collection_interval": 60
+			}
+		}
+	}
+}
+EOF
+
+# Register AWS Unified Cloud Watch Agent as a service and start it
+
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
+systemctl start amazon-cloudwatch-agent.service
+
+# Set up Flexd service
 
 cat > /etc/systemd/system/docker.flexd.service << EOF
 [Unit]
@@ -71,15 +155,19 @@ TimeoutStartSec=0
 Restart=always
 ExecStart=/usr/bin/docker run -p ${launch.albApiPort}:${launch.apiPort} -p ${launch.albLogPort}:${
       launch.logPort
-    } --name flexd -rm --env-file /etc/systemd/system/docker.flexd.env ${account}.dkr.ecr.${region}.amazonaws.com/${repo}:${tag}
+    } --name flexd --rm --env-file /etc/systemd/system/docker.flexd.env ${account}.dkr.ecr.${region}.amazonaws.com/${repo}:${tag}
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
+# Drop Flexd configuration file
+
 cat > /etc/systemd/system/docker.flexd.env << EOF
 ${require('fs').readFileSync(require('path').join(__dirname, '../../../../.aws.' + deploymentName + '.env'), 'utf8')}
 EOF
+
+# Start Flexd service
 
 systemctl start docker.flexd`;
 
