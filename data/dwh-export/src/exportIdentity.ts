ac1f46eaa5a8f2c560247287cf342831ec889a45
Tomasz Janczuk <tomasz@janczuk.org>
2019-04-18 13:00:43 -0700

    update BQ identity table schema

--- data/dwh-export/src/exportIdentity.ts
+++ data/dwh-export/src/exportIdentity.ts
@@ -9,11 +9,11 @@ export async function exportIdentity(ctx: IExportContext, config: IExportConfig)
         accountId: x.accountId && x.accountId.S,
         agentId: x.agentId && x.agentId.S,
         issuerId: x.iss && x.iss.S,
-        sub: x.sub && x.sub.S,
+        subject: x.sub && x.sub.S,
       },
     };
     item.insertId = `${item.json.deploymentId}/${item.json.accountId}/${item.json.agentId}/${item.json.issuerId}/${
-      item.json.sub
+      item.json.subject
     }/${item.json.ts}`;
     return item;
   });
