943e212a8b107c0ababe3deadb1fbe7de993f3b7
Randall Tombaugh <randall@fivequarters.io>
2019-07-09 15:45:30 -1000

    cli profile get/set cmd polish

---
+++ cli/fusebit-cli/src/commands/profile/ProfileGetCommand.ts
@@ -0,0 +1,49 @@
+import { Command, IExecuteInput } from '@5qtrs/cli';
+import { Text } from '@5qtrs/text';
+import { ProfileService, ExecuteService } from '../../services';
+
+// ------------------
+// Internal Constants
+// ------------------
+
+const command = {
+  name: 'Get Default Profile',
+  cmd: 'get',
+  summary: 'Get the default profile',
+  description: Text.create('Gets the current stored profile used as the default profile.'),
+  options: [
+    {
+      name: 'output',
+      aliases: ['o'],
+      description: "The format to display the output: 'pretty', 'json'",
+      default: 'pretty',
+    },
+  ],
+};
+
+// ----------------
+// Exported Classes
+// ----------------
+
+export class ProfileGetCommand extends Command {
+  private constructor() {
+    super(command);
+  }
+
+  public static async create() {
+    return new ProfileGetCommand();
+  }
+
+  protected async onExecute(input: IExecuteInput): Promise<number> {
+    const profileService = await ProfileService.create(input);
+    const executeService = await ExecuteService.create(input);
+
+    await executeService.newLine();
+
+    const profile = await profileService.getDefaultProfileOrThrow();
+
+    await profileService.displayProfile(profile);
+
+    return 0;
+  }
+}
