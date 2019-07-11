1574ccaf3ec049d3d2e8e67a31c7c9e0c54fe577
Randall Tombaugh <randall@fivequarters.io>
2019-07-11 12:51:37 -1000

    cli function edit cmd polish

--- cli/fusebit-cli/src/commands/function/FunctionEditCommand.ts
+++ cli/fusebit-cli/src/commands/function/FunctionEditCommand.ts
@@ -1,27 +1,28 @@
-import { Command, IExecuteInput, Message } from '@5qtrs/cli';
-import { ProfileService, ExecuteService, tryGetFusebit, getProfileSettingsFromFusebit } from '../../services';
-import * as http from 'http';
-import open from 'open';
-import { Text } from '@5qtrs/text';
+import { Command, IExecuteInput } from '@5qtrs/cli';
+import { ExecuteService, FunctionService } from '../../services';
 
 export class FunctionEditCommand extends Command {
   private constructor() {
     super({
       name: 'Edit Function',
       cmd: 'edit',
-      summary: 'Edit a function in the Fusebit Editor',
-      description: `Opens the Fusebit Editor in your default browser to edit a function. If the function does 
-not exist, it is created.`,
-      options: [
+      summary: 'Edit a function in the Fusebit editor',
+      description: [
+        'Opens the Fusebit Editor in your default browser to edit a function.',
+        'If the function does not exist, it is created.',
+      ].join(' '),
+      arguments: [
         {
           name: 'function',
-          aliases: ['f'],
-          description: 'The id of the function to edit.',
+          description: 'The id of the function to edit',
+          required: false,
         },
+      ],
+      options: [
         {
           name: 'theme',
           aliases: ['t'],
-          description: 'The theme of the editor: light (default) or dark.',
+          description: "The theme of the editor: 'light' or 'dark'",
           default: 'light',
         },
       ],
@@ -32,124 +33,17 @@ not exist, it is created.`,
     return new FunctionEditCommand();
   }
 
-  attempts: number = 0;
-
   protected async onExecute(input: IExecuteInput): Promise<number> {
-    await input.io.writeLine();
+    const functionId = input.arguments[0] as string;
+    const theme = input.options.theme as string;
 
-    let profileService = await ProfileService.create(input);
     const executeService = await ExecuteService.create(input);
-    let profile = await profileService.getExecutionProfile(
-      ['subscription', 'boundary', 'function'],
-      getProfileSettingsFromFusebit(tryGetFusebit())
-    );
+    const functionService = await FunctionService.create(input);
 
-    await executeService.execute(
-      {
-        header: 'Edit Function',
-        message: Text.create(
-          'Editing function ',
-          Text.bold(`${profile.boundary}/${profile.function}`),
-          ' on account ',
-          Text.bold(profile.account || ''),
-          ' and subscription ',
-          Text.bold(profile.subscription || ''),
-          '.'
-        ),
-        errorHeader: 'Edit Function Error',
-        errorMessage: 'Unable to edit the function',
-      },
-      async () => {
-        let port = 8000 + Math.floor(Math.random() * 100);
-        let editorHtml = getEditorHtml(port, profile, input);
-        return new Promise((resolve, reject) => {
-          http
-            .createServer((req, res) => {
-              if (req.method === 'GET') {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                return res.end(editorHtml);
-              } else {
-                res.writeHead(404);
-                return res.end();
-              }
-            })
-            .on('error', () => {
-              this.attempts++;
-              if (this.attempts > 10) {
-                reject(new Error('Unable to find a free port in the 80xx range to host a local service. Try again.'));
-                return;
-              } else {
-                this.onExecute(input);
-                return;
-              }
-            })
-            .listen(port, async () => {
-              const message = await Message.create({
-                message: Text.create([
-                  'Hosting the Fusebit Editor at ',
-                  Text.bold(`http://127.0.0.1:${port}`),
-                  Text.eol(),
-                  'If the browser does not open automatically, navigate to this URL.',
-                  Text.eol(),
-                  Text.eol(),
-                  'Ctrl-C to terminate...',
-                ]),
-              });
-              await message.write(input.io);
-              open(`http://127.0.0.1:${port}`);
-            });
-        });
-      }
-    );
+    await executeService.newLine();
+
+    await functionService.startEditServer(functionId, theme);
 
     return 0;
   }
-}
-
-function getEditorHtml(port: number, profile: any, input: IExecuteInput): string {
-  return `<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8" />
-
-    <link rel="icon" type="image/png" sizes="32x32" href="https://fusebit.io/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="https://fusebit.io/favicon-16x16.png" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-
-    <title>Fusebit ${profile.function}</title>
-
-    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-    <meta name="viewport" content="width=device-width" />
-
-    <style>
-        html,body {
-            width: 100%;
-            height: 100%;
-        }
-    </style>
-
-</head>
-<body>
-    <div id="editor" style="width:800px;height:500px;margin-top:30px;margin-left:auto;margin-right:auto">
-</body>
-
-<script src="https://cdn.fusebit.io/fusebit/js/fusebit-editor/latest/fusebit-editor.min.js"></script>
-<script type="text/javascript">
-  fusebit.createEditor(document.getElementById('editor'), '${profile.boundary}', '${profile.function}', {
-      accountId: '${profile.account}',
-      subscriptionId: '${profile.subscription}',
-      baseUrl: '${profile.baseUrl}',
-      accessToken: '${profile.accessToken}',
-  }, {
-      template: {},
-      editor: {
-        theme: '${input.options.theme}',
-      },
-  }).then(editorContext => {
-      editorContext.setFullScreen(true);
-  });
-</script>
-
-</html>  
-`;
 }
