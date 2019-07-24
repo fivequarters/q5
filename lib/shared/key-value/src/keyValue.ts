e9fa4848cf4d308f3ec0ee112a25c12140a0a398
Randall Tombaugh <randall@fivequarters.io>
2019-07-24 12:04:28 -1000

    key-value package update feature

--- lib/shared/key-value/src/keyValue.ts
+++ lib/shared/key-value/src/keyValue.ts
@@ -1,27 +1,71 @@
 import { EOL } from 'os';
+import { KeyValueException } from './KeyValueException';
+import { isObject, isString, isNumber, isBoolean } from '@5qtrs/type';
 
 // ------------------
 // Internal Constants
 // ------------------
 
-const keyValueRegex = /^\s*([^=]+?)\s*=\s*(.*?)\s*$/;
+const keyValueRegex = /^(\s*)([^=]+?)(\s*)=(\s*)(.*?)(\s*)$/;
 const commentLineRegex = /^\s*\#/;
+
+// ------------------
+// Internal Functions
+// ------------------
+
+function parseWithMap(data: string) {
+  const keyValues: KeyValues = {};
+  const keyValuesToLineMap: KeyValues = {};
+  const lines = data ? data.split(EOL) : [];
+
+  let index = 0;
+  for (const line of lines) {
+    if (!commentLineRegex.test(line)) {
+      const match = line.match(keyValueRegex);
+      if (match) {
+        const key = match[2];
+        const value = match[5];
+        keyValues[key] = value;
+        keyValuesToLineMap[key] = index;
+      }
+    }
+    index++;
+  }
+
+  return { keyValues, keyValuesToLineMap, lines };
+}
 
 // --------------
 // Exported Types
 // --------------
 
-export type KeyValue = { [property: string]: string | number | undefined };
+export type KeyValues = { [property: string]: string | number | boolean | undefined };
+
+export interface StructuredKeyValues {
+  values?: KeyValues;
+  serialized?: string;
+}
 
 // ------------------
 // Exported Functions
 // ------------------
 
-export function serialize(keyValues: KeyValue) {
+export function serialize(keyValues: KeyValues) {
+  if (!isObject(keyValues)) {
+    throw KeyValueException.valuesNotAnObject();
+  }
+
   const lines: string[] = [];
   for (const key of Object.keys(keyValues).sort()) {
-    const value = keyValues[key];
-    if (value) {
+    let value = keyValues[key];
+    if (value !== undefined) {
+      if (!isString(value)) {
+        if (isNumber(value) || isBoolean(value)) {
+          value = value.toString();
+        } else {
+          throw KeyValueException.valueNotAString(key);
+        }
+      }
       lines.push(`${key}=${value}`);
     }
   }
@@ -30,17 +74,58 @@ export function serialize(keyValues: KeyValue) {
 }
 
 export function parse(data: string) {
-  const keyValues: { [property: string]: string | number } = {};
-  const lines = data.split(EOL);
+  const parsed = parseWithMap(data);
+  return parsed.keyValues;
+}
 
-  for (const line of lines) {
-    if (!commentLineRegex.test(line)) {
-      const match = line.match(keyValueRegex);
-      if (match) {
-        keyValues[match[1]] = match[2];
+export function isEqual(keyValues1: KeyValues, keyValues2: KeyValues) {
+  return serialize(keyValues1) === serialize(keyValues2);
+}
+
+export function update(previousSeralized: string, current: StructuredKeyValues) {
+  if (!current.values) {
+    const serialized = current.serialized !== undefined ? current.serialized : previousSeralized;
+    return { values: parse(serialized), serialized };
+  }
+
+  if (current.serialized) {
+    if (current.serialized !== previousSeralized) {
+      const currentValues = parse(current.serialized);
+      if (isEqual(currentValues, current.values)) {
+        return current;
       }
+      if (!isEqual(parse(previousSeralized), current.values)) {
+        throw KeyValueException.serializedAndValuesUpdated(Object.keys(current.values));
+      }
+      return { values: currentValues, serialized: current.serialized };
     }
   }
 
-  return keyValues;
+  const previousParsed = parseWithMap(previousSeralized);
+  const lines = previousParsed.lines;
+  const linesToRemove = [];
+  for (const key of Object.keys(previousParsed.keyValues)) {
+    const currentValue = current.values[key];
+    if (currentValue === undefined) {
+      linesToRemove.push(previousParsed.keyValuesToLineMap[key]);
+    } else if (currentValue !== previousParsed.keyValues[key]) {
+      const lineToUpdate = previousParsed.keyValuesToLineMap[key] as number;
+      lines[lineToUpdate] = lines[lineToUpdate].replace(keyValueRegex, `$1$2$3=$4${currentValue}$6`);
+    }
+  }
+
+  const sortedLinesToRemove = linesToRemove.sort();
+  while (sortedLinesToRemove.length) {
+    const indexToRemove = sortedLinesToRemove.pop() as number;
+    lines.splice(indexToRemove, 1);
+  }
+
+  for (const key of Object.keys(current.values)) {
+    const currentValue = current.values[key];
+    if (currentValue !== undefined && previousParsed.keyValues[key] === undefined) {
+      lines.push(`${key}=${currentValue}`);
+    }
+  }
+
+  return { values: current.values, serialized: lines.join(EOL) };
 }
--- lib/shared/key-value/test/keyValue.test.ts
+++ lib/shared/key-value/test/keyValue.test.ts
@@ -1,4 +1,4 @@
-import { parse, serialize } from '../src';
+import { parse, serialize, update, KeyValueExceptionCode } from '../src';
 
 describe('serialize', () => {
   it('should properly serialize a key-value object', () => {
@@ -27,5 +27,205 @@ describe('parse', () => {
   it('should properly parse a key-value string even with comment lines', () => {
     const actual = parse('# this a comment\na=hello\nb=world\n#comment\nc=5');
     expect(actual).toEqual({ a: 'hello', c: '5', b: 'world' });
+  });
+});
+
+describe('update', () => {
+  it('should properly update when the serialized property is updated and values is undefined', () => {
+    const previous = 'key1=value1\n#comment\nkey2=value2';
+    const current = {
+      serialized: 'key1=value1\n#comment\nkey2=value2-updated\n#another comment',
+    };
+    const updated = update(previous, current);
+    expect(updated.serialized).toBe(current.serialized);
+    expect(updated.values).toEqual({ key1: 'value1', key2: 'value2-updated' });
+  });
+
+  it('should properly update when the value property is updated and serialized is undefined', () => {
+    const previous = 'key1=value1\n#comment\nkey2=value2';
+    const current = {
+      values: { key1: 'value1', key2: 'value2-updated' },
+    };
+    const updated = update(previous, current);
+    expect(updated.serialized).toBe('key1=value1\n#comment\nkey2=value2-updated');
+    expect(updated.values).toEqual(current.values);
+  });
+
+  it('should properly update when the serialized property is updated and values is unchanged', () => {
+    const previous = 'key1=value1\n#comment\nkey2=value2';
+    const current = {
+      values: { key1: 'value1', key2: 'value2' },
+      serialized: 'key1=value1\n#comment\nkey2=value2-updated\n#another comment',
+    };
+    const updated = update(previous, current);
+    expect(updated.serialized).toBe(current.serialized);
+    expect(updated.values).toEqual({ key1: 'value1', key2: 'value2-updated' });
+  });
+
+  it('should properly update when the value property is updated and serialized is unchanged', () => {
+    const previous = 'key1=value1\n#comment\nkey2=value2';
+    const current = {
+      values: { key1: 'value1', key2: 'value2-updated' },
+      serialized: 'key1=value1\n#comment\nkey2=value2',
+    };
+    const updated = update(previous, current);
+    expect(updated.serialized).toBe('key1=value1\n#comment\nkey2=value2-updated');
+    expect(updated.values).toEqual(current.values);
+  });
+
+  it('should properly update when the serialized property removes a key', () => {
+    const previous = 'key1=value1\n#comment\nkey2=value2';
+    const current = {
+      values: { key1: 'value1', key2: 'value2' },
+      serialized: 'key1=value1\n#comment has changed',
+    };
+    const updated = update(previous, current);
+    expect(updated.serialized).toBe(current.serialized);
+    expect(updated.values).toEqual({ key1: 'value1' });
+  });
+
+  it('should properly update when the values property removes a key', () => {
+    const previous = 'key1=value1\n#comment\nkey2=value2';
+    const current = {
+      values: { key1: 'value1' },
+      serialized: 'key1=value1\n#comment\nkey2=value2',
+    };
+    const updated = update(previous, current);
+    expect(updated.serialized).toBe('key1=value1\n#comment');
+    expect(updated.values).toEqual({ key1: 'value1' });
+  });
+
+  it('should properly update when the serialized property adds a key', () => {
+    const previous = 'key1=value1\n#comment\nkey2=value2';
+    const current = {
+      values: { key1: 'value1', key2: 'value2' },
+      serialized: 'key1=value1\n#comment has changed\nkey2=value2\n\n\n# comment again\nkey3 = value3',
+    };
+    const updated = update(previous, current);
+    expect(updated.serialized).toBe(current.serialized);
+    expect(updated.values).toEqual({ key1: 'value1', key2: 'value2', key3: 'value3' });
+  });
+
+  it('should properly update when the values property adds a key', () => {
+    const previous = 'key1=value1\n#comment\nkey2=value2';
+    const current = {
+      values: { key1: 'value1', key2: 'value2', key3: 'value3' },
+      serialized: 'key1=value1\n#comment\nkey2=value2',
+    };
+    const updated = update(previous, current);
+    expect(updated.serialized).toBe('key1=value1\n#comment\nkey2=value2\nkey3=value3');
+    expect(updated.values).toEqual({ key1: 'value1', key2: 'value2', key3: 'value3' });
+  });
+
+  it('should properly update when the serialized property replaces a key', () => {
+    const previous = 'key1=value1\n#comment\nkey2=value2';
+    const current = {
+      values: { key1: 'value1', key2: 'value2' },
+      serialized: 'key1=value1\n#comment has changed\nkey3=value3\n\n\n# comment again\n',
+    };
+    const updated = update(previous, current);
+    expect(updated.serialized).toBe(current.serialized);
+    expect(updated.values).toEqual({ key1: 'value1', key3: 'value3' });
+  });
+
+  it('should properly update when the values property replaces a key', () => {
+    const previous = 'key1=value1\n#comment\nkey2=value2';
+    const current = {
+      values: { key1: 'value1', key3: 'value3' },
+      serialized: 'key1=value1\n#comment\nkey2=value2',
+    };
+    const updated = update(previous, current);
+    expect(updated.serialized).toBe('key1=value1\n#comment\nkey3=value3');
+    expect(updated.values).toEqual({ key1: 'value1', key3: 'value3' });
+  });
+
+  it('should properly update when the values property contains a key with an undefined value', () => {
+    const previous = 'key1=value1\n#comment\nkey2=value2';
+    const current = {
+      values: { key1: 'value1', key2: undefined },
+      serialized: 'key1=value1\n#comment\nkey2=value2',
+    };
+    const updated = update(previous, current);
+    expect(updated.serialized).toBe('key1=value1\n#comment');
+    expect(updated.values).toEqual({ key1: 'value1' });
+  });
+
+  it('should properly update when the values property contains a key with an empty string', () => {
+    const previous = 'key1=value1\n#comment\nkey2=value2';
+    const current = {
+      values: { key1: 'value1', key2: '' },
+      serialized: 'key1=value1\n#comment\nkey2=value2',
+    };
+    const updated = update(previous, current);
+    expect(updated.serialized).toBe('key1=value1\n#comment\nkey2=');
+    expect(updated.values).toEqual({ key1: 'value1', key2: '' });
+  });
+
+  it('should properly handle empty strings and undefined values', () => {
+    const previous = '';
+    const current = {
+      values: { key1: undefined },
+      serialized: '',
+    };
+    const updated = update(previous, current);
+    expect(updated.serialized).toBe('');
+    expect(updated.values).toEqual({});
+  });
+
+  it('should properly update when both the values and serialized have changed in the same way', () => {
+    const previous = 'key1=value1\n#comment\nkey2=value2';
+    const current = {
+      values: { key1: 'value1', key2: 'value2-updated' },
+      serialized: 'key1=value1\n#comment has also changed\nkey2=value2-updated',
+    };
+
+    const updated = update(previous, current);
+    expect(updated.serialized).toBe('key1=value1\n#comment has also changed\nkey2=value2-updated');
+    expect(updated.values).toEqual(current.values);
+  });
+
+  it('should properly update when both the values and serialized have changed in the same way with number values', () => {
+    const previous = 'key1=value1\n#comment\nkey2=4';
+    const current = {
+      values: { key1: 'value1', key2: 5 },
+      serialized: 'key1=value1\n#comment has also changed\nkey2=5',
+    };
+
+    const updated = update(previous, current);
+    expect(updated.serialized).toBe('key1=value1\n#comment has also changed\nkey2=5');
+    expect(updated.values).toEqual(current.values);
+  });
+
+  it('should properly update when both the values and serialized have changed and booleans are involved', () => {
+    const previous = 'memorySize=128\ntimeout=30\nstaticIp=false';
+    const current = {
+      values: {
+        memorySize: 128,
+        timeout: 60,
+        staticIp: false,
+      },
+      serialized: 'memorySize=128\ntimeout=60\nstaticIp=false',
+    };
+
+    const updated = update(previous, current);
+    expect(updated.serialized).toBe('memorySize=128\ntimeout=60\nstaticIp=false');
+    expect(updated.values).toEqual(current.values);
+  });
+
+  it('should throw an exception if both the values and serialized have changed', () => {
+    const previous = 'key1=value1\n#comment\nkey2=value2';
+    const current = {
+      values: { key1: 'value1', key2: 'value2-updated-a' },
+      serialized: 'key1=value1\n#comment\nkey2=value2-updated-b',
+    };
+
+    let error;
+    try {
+      update(previous, current);
+    } catch (err) {
+      error = err;
+    }
+    expect(error).toBeDefined();
+    expect(error.code).toBe(KeyValueExceptionCode.serializedAndValuesUpdated);
   });
 });
