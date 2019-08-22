6efede68aaf58e5464fc3c1f147f2f897276f7f0
Randall Tombaugh <randall@fivequarters.io>
2019-08-22 07:36:12 -1000

    fusebit-mark component

---
+++ comp/fusebit-mark/src/demo.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { FusebitMark } from './FusebitMark';
+import { Box } from '@5qtrs/box';
+import { FusebitColor } from '@5qtrs/fusebit-color';
+
+const App = () => (
+  <Box>
+    <FusebitMark margin={20} />
+    <FusebitMark size={75} margin={20} color={FusebitColor.cyan} />
+    <FusebitMark size={100} margin={20} color={FusebitColor.orange} />
+    <FusebitMark size={150} margin={20} color={FusebitColor.dark} />
+    <FusebitMark expand />
+  </Box>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
