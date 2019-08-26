063ae17ad10fcec4d1d2744dfe1e99c2cddbe63c
Tomasz Janczuk <tomasz@janczuk.org>
2019-08-26 12:10:39 -0700

    improve auth0 image, update blog date

--- site/fusebit-site/src/page/blog/posts/index.ts
+++ site/fusebit-site/src/page/blog/posts/index.ts
@@ -2,6 +2,6 @@ import NewTrend from './2019-06-08-NewTrend';
 import SchedulingRequests from './2019-06-27-SchedulingRequests';
 import BuyBuild from './2019-07-04-BuyBuild';
 import IntegrationLandscape from './2019-08-19-IntegrationLandscape';
-import InterviewAuth0 from './2019-08-22-InterviewAuth0';
+import InterviewAuth0 from './2019-08-26-InterviewAuth0';
 
 export const posts = [InterviewAuth0, IntegrationLandscape, BuyBuild, SchedulingRequests, NewTrend];
