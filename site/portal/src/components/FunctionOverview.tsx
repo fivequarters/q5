180dc2eb4719f872fbe36abf28c4a9301273255c
Tomasz Janczuk <tomasz@janczuk.org>
2019-12-18 15:34:16 -0800

    FunctionCode component with full screen modal view

--- site/portal/src/components/FunctionOverview.tsx
+++ site/portal/src/components/FunctionOverview.tsx
@@ -1,30 +1,43 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router-dom";
 // import { makeStyles } from "@material-ui/core/styles";
 // import { useProfile } from "./ProfileProvider";
-// import Grid from "@material-ui/core/Grid";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 // import Paper from "@material-ui/core/Paper";
 // import Tabs from "@material-ui/core/Tabs";
 // import Tab from "@material-ui/core/Tab";
 // import { FusebitError } from "./ErrorBoundary";
 
-// const useStyles = makeStyles(theme => ({
-//   gridContainer: {
-//     marginTop: 12,
-//     paddingLeft: theme.spacing(2),
-//     paddingRight: theme.spacing(2)
-//   },
-//   gridLine: {
-//     marginTop: theme.spacing(1),
-//     marginBottom: theme.spacing(1)
-//   }
-// }));
+const useStyles = makeStyles(theme => ({
+  gridContainer: {
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    marginBottom: theme.spacing(2)
+  }
+}));
 
 function FunctionOverview({ data, match }: any) {
+  const history = useHistory();
+  const classes = useStyles();
   // const { profile } = useProfile();
   // const classes = useStyles();
   // const { params } = match;
 
-  return <div>[TODO: FunctionOverview]</div>;
+  const handleEditCode = () => {
+    history.replace("code");
+  };
+
+  return (
+    <Grid container className={classes.gridContainer}>
+      <Grid item xs={12}>
+        <Button color="primary" variant="contained" onClick={handleEditCode}>
+          Edit Code
+        </Button>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default FunctionOverview;
