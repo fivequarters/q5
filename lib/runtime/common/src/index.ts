a8eda2756f8e9cf2f19cf90bb141cd1eb1aad057
Benn Bollay <bb.github@magitech.org>
2020-04-01 22:52:43 -0700

    checkpoint

---
+++ lib/runtime/common/src/index.ts
@@ -0,0 +1,10 @@
+// @ts-ignore
+import { invoke_function } from './invoke_function.js';
+// @ts-ignore
+import { dispatch_event } from './analytics.js';
+
+export {
+  invoke_function,
+  dispatch_event,
+  logging,
+};
