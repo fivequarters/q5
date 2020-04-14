1669a5befdeae9173a35a9e4fb21c77c58533d39
Benn Bollay <bb.github@magitech.org>
2020-04-14 11:23:13 -0700

    checkpoint

--- site/portal/src/components/Monitoring.tsx
+++ site/portal/src/components/Monitoring.tsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import SubscriptionActivity from "./SubscriptionActivity";
-import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
-import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
+import ToggleButton from "@material-ui/lab/ToggleButton";
 import ms from "ms";
+import { useProfile } from "./ProfileProvider";
 
 // Duplicated; not sure how to declare externally.
 enum BucketWidths {
@@ -17,93 +17,144 @@ enum BucketWidths {
   Week = "1w",
   Month = "1M",
   Quarter = "1q",
-  Year = "1y",
-};
+  Year = "1y"
+}
 
 interface IDateInterval {
   width: BucketWidths;
   timeStart: Date;
   timeEnd: Date;
-};
+}
 
 interface IParams {
   accountId?: string;
   subscriptionId?: string;
   boundaryId?: string;
   functionId?: string;
-};
+}
 
 interface IProps {
   params: IParams;
-};
+}
 
 interface TabPanelProps {
   children?: React.ReactNode;
   index: any;
-  value: any;
+  graphIndex: any;
 }
+
+const availableGraphs = [
+  {
+    label: "HTTP Responses",
+    code: "codeactivityhg"
+  },
+  {
+    label: "HTTP Latency",
+    code: "codelatencyhg"
+  }
+];
 
 const TabPanel = (props: TabPanelProps) => {
-  const { children, value, index, ...other } = props;
+  const { children, graphIndex, index, ...other } = props;
 
   return (
-    <div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`} {...other}>
-      {value === index && <Box p={3}>{children}</Box>}
+    <div role="tabpanel" hidden={graphIndex !== index} id={`simple-tabpanel-${index}`} {...other}>
+      {graphIndex === index && <Box p={3}>{children}</Box>}
     </div>
   );
-}
+};
 
-const MonitoringPanel: React.FC<IProps> = (props) => {
-  const [value, setValue] = useState(0);
-  const [ interval, setInterval ] = useState<IDateInterval>({timeStart: new Date(Date.now() - ms('7d')), timeEnd: new Date(), width: BucketWidths.Day});
-  const [ eventRange, setEventRange ] = useState<IDateInterval|null>(null);
-  const [ activeCodeList, setActiveCodeList ] = useState<any>([200, 300, 400]);
-  const [ activeCode, setActiveCode ] = useState<number|null>(200);
+const MonitoringPanel: React.FC<IProps> = props => {
+  const { profile } = useProfile();
+  const [graphIndex, setGraphIndex] = useState(0);
+  const [interval, setInterval] = useState<IDateInterval>({
+    timeStart: new Date(Date.now() - ms("7d")),
+    timeEnd: new Date(),
+    width: BucketWidths.Day
+  });
+  const [eventRange, setEventRange] = useState<IDateInterval | null>(null);
+  const [activeCodeList, setActiveCodeList] = useState<any>([200, 300, 400]);
+  const [activeCode, setActiveCode] = useState<number | null>(200);
 
+  // Create a common URL wart to pass to the sub-components
+  const params = props.params;
+
+  let warts = [
+    params.accountId ? `account/${params.accountId}` : "",
+    params.subscriptionId ? `subscription/${params.subscriptionId}` : "",
+    params.boundaryId ? `boundary/${params.boundaryId}` : "",
+    params.functionId ? `function/${params.functionId}` : ""
+  ].filter(x => x);
+
+  const urlWart = `${profile.baseUrl}/v1/` + warts.join("/");
+
+  // Return the div.
   return (
     <div>
       {/* Create the button group, one button for each of the different bucket sizes. */}
-      <ToggleButtonGroup size="small" exclusive={true} onChange={(e, v) => setInterval({...interval, width: v})} value={interval.width}>
-        {
-          Object.keys(BucketWidths).map((width) => {
-            let tWidth: keyof typeof BucketWidths = width as keyof typeof BucketWidths;
-            return (<ToggleButton key={width} value={BucketWidths[tWidth]}>{width}</ToggleButton>);
-          })
-        }
+      <ToggleButtonGroup
+        size="small"
+        exclusive={true}
+        onChange={(e, v) => setInterval({ ...interval, width: v })}
+        value={interval.width}
+      >
+        {Object.keys(BucketWidths).map(width => {
+          let tWidth: keyof typeof BucketWidths = width as keyof typeof BucketWidths;
+          return (
+            <ToggleButton key={width} value={BucketWidths[tWidth]}>
+              {width}
+            </ToggleButton>
+          );
+        })}
       </ToggleButtonGroup>
 
       {/* Show the tab bar for each of the different visualizations */}
       <AppBar position="static">
-        <Tabs value={value} onChange={(e, v) => setValue(v)}>
+        <Tabs value={graphIndex} onChange={(e, v) => setGraphIndex(v)}>
           <Tab label="Summary" />
-          <Tab label="Results" />
-          <Tab label="Latency" />
+          {availableGraphs.map(graph => {
+            return <Tab key={graph.label} label={graph.label} />;
+          })}
         </Tabs>
       </AppBar>
-      <TabPanel value={value} index={0}>
+      <TabPanel graphIndex={graphIndex} index={0}>
         Item One
       </TabPanel>
 
-      {/* Render the various graphs and pages. */}
-      <TabPanel value={value} index={1}>
-        <SubscriptionActivity code="codeactivityhg" label="HTTP Responses" params={props.params} interval={interval} setEventRange={setEventRange} setActiveCodeList={setActiveCodeList} />
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <SubscriptionActivity code="codelatencyhg" label="HTTP Latency" params={props.params} interval={interval} setEventRange={setEventRange} setActiveCodeList={setActiveCodeList} />
-      </TabPanel>
-      <ToggleButtonGroup size="small" exclusive={true} onChange={(e, v) => setActiveCode(v)} value={activeCode}>
-        {
-          activeCodeList.map((code:number) => {
-            return (<ToggleButton key={code} value={code}>{code}</ToggleButton>);
-          })
-        }
+      {/* Show the various graphs available. */}
+      {availableGraphs.map((graph, n) => {
+        return (
+          <TabPanel key={n + 1} graphIndex={graphIndex} index={n + 1}>
+            <SubscriptionActivity
+              code={graph.code}
+              label={graph.label}
+              urlWart={urlWart}
+              profile={profile}
+              interval={interval}
+              setEventRange={setEventRange}
+              setActiveCodeList={setActiveCodeList}
+            />
+          </TabPanel>
+        );
+      })}
+
+      {/* Display the events that occurred in the selected time period. */}
+      <ToggleButtonGroup
+        size="small"
+        exclusive={true}
+        onChange={(e, v) => setActiveCode(v)}
+        value={activeCode}
+      >
+        {activeCodeList.map((code: number) => {
+          return (
+            <ToggleButton key={code} value={code}>
+              {code}
+            </ToggleButton>
+          );
+        })}
       </ToggleButtonGroup>
       {/*<HTTPEventLogList params={props.params} interval={interval} activeCode={activeCode} /> */}
     </div>
   );
-}
-// XXX probably also need to tweak the timestamp callback; I don't think that's working quite right.  Or maybe
-// it's just unused; hard to tell at the moment.   Yeah, it's just unused.
-export {
-  MonitoringPanel
 };
+export { MonitoringPanel };
--- site/portal/src/components/SubscriptionActivity.tsx
+++ site/portal/src/components/SubscriptionActivity.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { ResponsiveContainer, LineChart, Line, CartesianGrid, Tooltip, XAxis, YAxis, Legend } from 'recharts';
+import { ResponsiveContainer, LineChart, Line, CartesianGrid, Tooltip, XAxis, YAxis, Legend } from "recharts";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Superagent from "superagent";
 import { useProfile } from "./ProfileProvider";
@@ -13,73 +13,70 @@ enum BucketWidths {
   Week = "1w",
   Month = "1M",
   Quarter = "1q",
-  Year = "1y",
-};
+  Year = "1y"
+}
 
 interface IDateInterval {
   width: BucketWidths;
   timeStart: Date;
   timeEnd: Date;
-};
-
-interface IParams {
-  accountId?: string;
-  subscriptionId?: string;
-  boundaryId?: string;
-  functionId?: string;
-};
+}
 
 interface IProps {
   code: string;
   label: string;
-  params: IParams;
+  urlWart: string;
+  profile: IFusebitProfile;
   interval: IDateInterval;
   setEventRange: (newInterval: IDateInterval) => void;
   setActiveCodeList: (newCodeList: number[]) => void;
-};
+}
 
 // Quick convienent map so everything isn't the same color.
 const codeColorMap = {
   200: "#ffb997",
   404: "#843b62",
-  501: "#0b032d",
+  501: "#0b032d"
 };
 
-const getData = async (props: IProps, profile: IFusebitProfile, range: IDateInterval, setData: any): Promise<void> => {
+const getData = async (
+  profile: IFusebitProfile,
+  code: string,
+  urlWart: string,
+  interval: IDateInterval,
+  setData: any,
+  setActiveCodeList: any
+): Promise<void> => {
   try {
-    let auth = await ensureAccessToken(profile);
-    let warts = [
-      props.params.accountId ? `account/${props.params.accountId}` : '',
-      props.params.subscriptionId ? `subscription/${props.params.subscriptionId}` : '',
-      props.params.boundaryId ? `boundary/${props.params.boundaryId}` : '',
-      props.params.functionId ? `function/${props.params.functionId}` : '',
-    ].filter(x => x);
+    const auth = await ensureAccessToken(profile);
 
     let result: any = await Superagent.get(
-      `${profile.baseUrl}/v1/` + warts.join('/') +
-      `/statistics/${props.code}/` +
-      `${range.timeStart.toISOString()}/${range.timeEnd.toISOString()}/${range.width}`
+      `${urlWart}/statistics/${code}/` +
+        `${interval.timeStart.toISOString()}/${interval.timeEnd.toISOString()}/${interval.width}`
     ).set("Authorization", `Bearer ${auth.access_token}`);
 
     // Make sure there's always a 0-value begin and end entry to track 'loading' state easily.
     if (result.body.data.length === 0) {
-      result.body.data = [{key: range.timeStart}, {key: range.timeEnd}];
+      result.body.data = [{ key: interval.timeStart }, { key: interval.timeEnd }];
     }
     setData(result.body);
-
+    setActiveCodeList(result.body.codes);
   } catch (e) {
     throw createHttpException(e);
   }
-}
+};
 
-const SubscriptionActivity: React.FC<IProps> = (props) => {
+const SubscriptionActivity: React.FC<IProps> = props => {
   const { profile } = useProfile();
-  const [ data, setData ] = useState({codes: [], data: []});
+  const [data, setData] = useState({ codes: [], data: [] });
 
-  // Run once on page load.
+  // Run once on page load, but first unpack props:
+  const { code, label, urlWart, interval, setEventRange, setActiveCodeList } = props;
+
   useEffect(() => {
-    getData(props, profile, props.interval, setData);
-  }, [profile, props]);
+    getData(profile, code, urlWart, interval, setData, setActiveCodeList);
+  }, [profile, code, urlWart, interval, setData, setActiveCodeList]);
+  // ^^^ Note to self: don't put props in the deps, it's not a stable referant.
 
   if (data.data.length === 0) {
     return <LinearProgress />;
@@ -87,47 +84,59 @@ const SubscriptionActivity: React.FC<IProps> = (props) => {
 
   const dateTickFormatter = (msTime: number): string => {
     return new Date(msTime).toISOString();
-  }
+  };
 
-	const CustomTooltip = ({ active, payload, label}:any) => {
-		if (active) {
-			return (
-				<div className="custom-tooltip">
-					<p className="label">{`${dateTickFormatter(label)}`}</p>
-					{
-				    payload.map((line:any) => {
-              return (<p key={line.name} className="desc">{line.name}: {line.value}</p>);
-            })
-          }
-				</div>
-			);
-		}
+  const CustomTooltip = ({ active, payload, label }: any) => {
+    if (active) {
+      return (
+        <div className="custom-tooltip">
+          <p className="label">{`${dateTickFormatter(label)}`}</p>
+          {payload.map((line: any) => {
+            return (
+              <p key={line.name} className="desc">
+                {line.name}: {line.value}
+              </p>
+            );
+          })}
+        </div>
+      );
+    }
 
-		return null;
-	};
-
-  props.setActiveCodeList(data.codes);
+    return null;
+  };
 
   // Quick hack, let's turn the key into an integer.
   return (
-    <div style={{ width: '100%', height: 300 }}>
+    <div style={{ width: "100%", height: 300 }}>
       <ResponsiveContainer>
-      
-        <LineChart width={900} height={500} data={data.data} onClick={ (e, v) => props.setEventRange(e.activeLabel) }>
+        <LineChart width={900} height={500} data={data.data} onClick={(e, v) => setEventRange(e.activeLabel)}>
           <CartesianGrid stroke="#ccc" />
           <Tooltip content={CustomTooltip} />
-          <Legend verticalAlign="top" height={36}/>
-          <XAxis type="number" domain={[props.interval.timeStart.getTime(), props.interval.timeEnd.getTime()]} dataKey="key" label={{value:props.label, position: "insideBottom"}} tickFormatter={dateTickFormatter}/>
+          <Legend verticalAlign="top" height={36} />
+          <XAxis
+            type="number"
+            domain={[interval.timeStart.getTime(), interval.timeEnd.getTime()]}
+            dataKey="key"
+            label={{ value: label, position: "insideBottom" }}
+            tickFormatter={dateTickFormatter}
+          />
           <YAxis />
-          {
-            data.codes.map((id) => {
-              return (<Line type="monotone" key={id} dataKey={id} dot={false} activeDot={{r:4}} stroke={codeColorMap[id]} />);
-            })
-          }
+          {data.codes.map(id => {
+            return (
+              <Line
+                type="monotone"
+                key={id}
+                dataKey={id}
+                dot={false}
+                activeDot={{ r: 4 }}
+                stroke={codeColorMap[id]}
+              />
+            );
+          })}
         </LineChart>
       </ResponsiveContainer>
     </div>
   );
-}
+};
 
 export default SubscriptionActivity;
