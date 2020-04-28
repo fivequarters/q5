9af62cd2ddc6dff36e1ed8645de791898342468d
Benn Bollay <bb.github@magitech.org>
2020-04-28 14:17:24 -0700

    Add the new FusebitMonitor.ts file

---
+++ site/portal/src/lib/FusebitMonitor.ts
@@ -0,0 +1,111 @@
+import { IFusebitProfile } from './Settings';
+import { ensureAccessToken, createHttpException } from './Fusebit';
+import Superagent from 'superagent';
+
+enum BucketWidths {
+  Minute = '1m',
+  Hour = '1h',
+  Day = '1d',
+  Week = '1w',
+  Month = '1M',
+  Quarter = '1q',
+  Year = '1y',
+}
+
+interface IDateIntervalType {
+  width: BucketWidths;
+  from: Date;
+  to: Date;
+}
+
+interface IGetDataOptionsType {
+  offset: number;
+  pageSize: number;
+  orderBy: string;
+  orderDir: string;
+}
+
+const Locale = 'en-US';
+
+const BucketWidthsDateFormat = {
+  [BucketWidths.Minute]: new Intl.DateTimeFormat(Locale, { hour: 'numeric', minute: 'numeric', second: 'numeric' }),
+  [BucketWidths.Hour]: new Intl.DateTimeFormat(Locale, { month: 'short', day: 'numeric', hour: 'numeric' }),
+  [BucketWidths.Day]: new Intl.DateTimeFormat(Locale, { month: 'short', day: 'numeric' }),
+  [BucketWidths.Week]: new Intl.DateTimeFormat(Locale, { month: 'short', day: 'numeric' }),
+  [BucketWidths.Month]: new Intl.DateTimeFormat(Locale, { year: '2-digit', month: 'short', day: 'numeric' }),
+  [BucketWidths.Quarter]: new Intl.DateTimeFormat(Locale, { year: '2-digit', month: 'short' }),
+  [BucketWidths.Year]: new Intl.DateTimeFormat(Locale, { year: '2-digit', month: 'short' }),
+};
+
+const getBulkMonitorData = async (
+  profile: IFusebitProfile,
+  urlWart: string,
+  interval: IDateInterval | null,
+  activeCode: number | null,
+  options: IGetDataOptions,
+  setData: any
+): Promise<void> => {
+  if (interval == null || activeCode == null) {
+    return setData({ items: [], total: 0 });
+  }
+
+  try {
+    const auth = await ensureAccessToken(profile);
+
+    let result: any = await Superagent.get(`${urlWart}/statistics/itemizedbulk`)
+      .query({
+        from: interval.from.toISOString(),
+        to: interval.to.toISOString(),
+        statusCode: activeCode,
+        next: options.offset,
+        count: options.pageSize,
+      })
+      .set('Authorization', `Bearer ${auth.access_token}`);
+
+    setData(result.body);
+  } catch (e) {
+    throw createHttpException(e);
+  }
+};
+
+const getStatisticalMonitorData = async (
+  profile: IFusebitProfile,
+  queryType: string,
+  urlWart: string,
+  interval: IDateInterval,
+  setData: any,
+  setActiveCodeList: any
+): Promise<void> => {
+  try {
+    const auth = await ensureAccessToken(profile);
+
+    let result: any = await Superagent.get(`${urlWart}/statistics/${queryType}`)
+      .query({
+        from: interval.from.toISOString(),
+        to: interval.to.toISOString(),
+        width: interval.width,
+      })
+      .set('Authorization', `Bearer ${auth.access_token}`);
+
+    // Make sure there's always a 0-value begin and end entry to track 'loading' state easily.
+    if (result.body.items.length === 0) {
+      result.body.items = [{ key: interval.from }, { key: interval.to }];
+    }
+
+    // Convert the ISO8601 strings into ms time, which the graphing libraries deal with better.
+    setData({
+      codes: result.body.codes,
+      items: result.body.items.map((e: any) => {
+        return { ...e, key: Date.parse(e.key) };
+      }),
+    });
+    setActiveCodeList(result.body.codes);
+  } catch (e) {
+    throw createHttpException(e);
+  }
+};
+
+export type IDateInterval = IDateIntervalType;
+export type IGetDataOptions = IGetDataOptionsType;
+
+export { BucketWidths, BucketWidthsDateFormat, getBulkMonitorData, getStatisticalMonitorData };
