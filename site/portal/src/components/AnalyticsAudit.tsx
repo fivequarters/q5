cf492c81459e696c9ae5176e06dee30a7884716c
Benn Bollay <bb.github@magitech.org>
2020-05-14 09:29:45 -0700

    Commit

--- site/portal/src/components/AnalyticsAudit.tsx
+++ site/portal/src/components/AnalyticsAudit.tsx
@@ -1,77 +1,68 @@
-import LinearProgress from "@material-ui/core/LinearProgress";
-import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import React from "react";
-import { Audit, AuditFilter } from "../lib/FusebitTypes";
-import { getUISettings, setUISettings } from "../lib/Settings";
-import ActivityFilterContent, {
-  defaultFilterAction,
-  defaultFilterFrom,
-} from "./ActivityFilterContent";
-import { AuditProvider, useAudit } from "./AuditProvider";
-import ExplorerTable, { HeadCell } from "./ExplorerTable";
-import PortalError from "./PortalError";
-import ResourceCrumb from "./ResourceCrumb";
-import TableInfoRow from "./TableInfoRow";
-import Chip from "@material-ui/core/Chip";
-import { useHistory } from "react-router-dom";
-import AgentTooltip, { IssuerSubjectAgent } from "./AgentTooltip";
-import { AgentState } from "./AgentProvider";
-import CancelIcon from "@material-ui/icons/Cancel";
-import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
+import { Audit, AuditFilter } from '../lib/FusebitTypes';
+import { getUISettings, setUISettings } from '../lib/Settings';
+import ActivityFilterContent, { defaultFilterAction, defaultFilterFrom } from './ActivityFilterContent';
+import { AuditProvider, useAudit } from './AuditProvider';
+import ExplorerTable, { HeadCell } from './ExplorerTable';
+import PortalError from './PortalError';
+import ResourceCrumb from './ResourceCrumb';
+import TableInfoRow from './TableInfoRow';
+import Chip from '@material-ui/core/Chip';
+import AgentTooltip, { IssuerSubjectAgent } from './AgentTooltip';
+import { AgentState } from './AgentProvider';
+import CancelIcon from '@material-ui/icons/Cancel';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import { useHashHistory } from './HashHistory';
 
 const useStyles = makeStyles((theme: any) => ({
   noWrap: {
-    whiteSpace: "nowrap",
+    whiteSpace: 'nowrap',
   },
   warning: {
     marginLeft: theme.spacing(2),
     marginRight: theme.spacing(2),
   },
   chips: {
-    display: "flex",
-    flexWrap: "wrap",
+    display: 'flex',
+    flexWrap: 'wrap',
   },
   chip: {
     marginRight: theme.spacing(1),
     marginBottom: theme.spacing(1),
   },
   access: {
-    display: "flex",
+    display: 'flex',
   },
 }));
 
-const pad = (i: number) => (i < 10 ? "0" + i : i.toString());
+const pad = (i: number) => (i < 10 ? '0' + i : i.toString());
 
-type ActivityImplProps = {
+type AnalyticsAuditImplProps = {
   filterMask: AuditFilter;
   actionFilter?: string[];
 };
 
-function ActivityImpl({ actionFilter, filterMask }: ActivityImplProps) {
+function AnalyticsAuditImpl({ actionFilter, filterMask }: AnalyticsAuditImplProps) {
   const [audit] = useAudit();
   const [utc, setUtc] = React.useState<boolean>(getUISettings().utcTime);
   const classes = useStyles();
-  const history = useHistory();
+  const [, setHistory] = useHashHistory('audit', {});
   const [agents, setAgents] = React.useState<IssuerSubjectAgent>({});
 
   const formatUtcDate = (d: Date) =>
-    `${d.getUTCFullYear()}-${pad(d.getUTCMonth() + 1)}-${pad(
-      d.getUTCDate()
-    )} ${pad(d.getUTCHours())}:${pad(d.getUTCMinutes())}:${pad(
-      d.getUTCSeconds()
-    )}`;
+    `${d.getUTCFullYear()}-${pad(d.getUTCMonth() + 1)}-${pad(d.getUTCDate())} ${pad(d.getUTCHours())}:${pad(
+      d.getUTCMinutes()
+    )}:${pad(d.getUTCSeconds())}`;
 
   const formatLocalDate = (d: Date) =>
-    `${d.getFullYear()}-${pad(d.getMonth() + 1)}-${pad(d.getDate())} ${pad(
-      d.getHours()
-    )}:${pad(d.getMinutes())}:${pad(d.getSeconds())}`;
+    `${d.getFullYear()}-${pad(d.getMonth() + 1)}-${pad(d.getDate())} ${pad(d.getHours())}:${pad(d.getMinutes())}:${pad(
+      d.getSeconds()
+    )}`;
 
-  const handleSetAgent = (
-    issuerId: string,
-    subject: string,
-    agent: AgentState
-  ) => {
+  const handleSetAgent = (issuerId: string, subject: string, agent: AgentState) => {
     if (!agents[issuerId]) {
       agents[issuerId] = {};
     }
@@ -81,10 +72,10 @@ function ActivityImpl({ actionFilter, filterMask }: ActivityImplProps) {
 
   const headCells: HeadCell<Audit>[] = [
     {
-      id: "authorized",
-      align: "left",
-      label: "Access",
-      render: (row) =>
+      id: 'authorized',
+      align: 'left',
+      label: 'Access',
+      render: row =>
         row.authorized ? (
           <CheckCircleIcon className={classes.access} />
         ) : (
@@ -92,31 +83,26 @@ function ActivityImpl({ actionFilter, filterMask }: ActivityImplProps) {
         ),
     },
     {
-      id: "resource",
-      align: "left",
-      label: "Resource",
-      render: (row) => (
-        <ResourceCrumb
-          resource={row.resource}
-          resourceMask={filterMask.resource}
-        />
-      ),
+      id: 'resource',
+      align: 'left',
+      label: 'Resource',
+      render: row => <ResourceCrumb resource={row.resource} resourceMask={filterMask.resource} />,
     },
     {
-      id: "action",
-      align: "left",
-      label: "Action",
-      render: (row) => (
+      id: 'action',
+      align: 'left',
+      label: 'Action',
+      render: row => (
         <Typography variant="inherit" className={classes.noWrap}>
           {row.action}
         </Typography>
       ),
     },
     {
-      id: "timestamp",
-      align: "left",
-      label: utc ? "Time (UTC)" : "Time (Local)",
-      render: (row) => {
+      id: 'timestamp',
+      align: 'left',
+      label: utc ? 'Time (UTC)' : 'Time (Local)',
+      render: row => {
         const d = new Date(row.timestamp);
         return utc ? (
           <Typography variant="inherit" className={classes.noWrap}>
@@ -130,51 +116,36 @@ function ActivityImpl({ actionFilter, filterMask }: ActivityImplProps) {
       },
     },
     {
-      id: "issuerId",
-      align: "left",
-      label: "Issuer ID",
+      id: 'issuerId',
+      align: 'left',
+      label: 'Issuer ID',
     },
     {
-      id: "subject",
-      align: "left",
-      label: "Subject",
-      render: (row) => (
-        <AgentTooltip
-          issuerId={row.issuerId}
-          subject={row.subject}
-          agents={agents}
-          onSetAgent={handleSetAgent}
-        >
+      id: 'subject',
+      align: 'left',
+      label: 'Subject',
+      render: row => (
+        <AgentTooltip issuerId={row.issuerId} subject={row.subject} agents={agents} onSetAgent={handleSetAgent}>
           <Typography variant="inherit">{row.subject}</Typography>
         </AgentTooltip>
       ),
     },
   ];
 
-  if (audit.status === "loading" || audit.status === "updating") {
+  if (audit.status === 'loading' || audit.status === 'updating') {
     return <LinearProgress />;
   }
 
-  if (audit.status === "error") {
+  if (audit.status === 'error') {
     return <PortalError error={audit.error} padding={true} />;
   }
 
   const updateFilter = (newFilter: AuditFilter) => {
-    let query: any = { ...newFilter };
-    delete query.count;
-    const hash = `#${Object.keys(query)
-      .reduce<string[]>((a: string[], k: string) => {
-        if (query[k] !== undefined) {
-          a.push(`${k}=${encodeURIComponent(query[k])}`);
-        }
-        return a;
-      }, [])
-      .join("&")}`;
-    history.replace(hash);
+    setHistory(newFilter);
   };
 
   const handleFilterReset = () => {
-    history.push("#");
+    setHistory({});
   };
 
   const handleFilterApply = (newFilter: AuditFilter, newUtc: boolean) => {
@@ -202,12 +173,11 @@ function ActivityImpl({ actionFilter, filterMask }: ActivityImplProps) {
   };
 
   function firstRowMessage() {
-    if (audit.status === "ready") {
+    if (audit.status === 'ready') {
       if (audit.data.data.length === 0) {
         return (
           <Typography variant="inherit">
-            No matching audit entries found. Please use the filter to relax the
-            search criteria.
+            No matching audit entries found. Please use the filter to relax the search criteria.
           </Typography>
         );
       }
@@ -215,8 +185,8 @@ function ActivityImpl({ actionFilter, filterMask }: ActivityImplProps) {
       if (audit.data.hasMore) {
         return (
           <TableInfoRow>
-            Showing only the first {audit.data.data.length} matching audit
-            entries. Please use the filter to narrow down the criteria.
+            Showing only the first {audit.data.data.length} matching audit entries. Please use the filter to narrow down
+            the criteria.
           </TableInfoRow>
         );
       }
@@ -229,57 +199,40 @@ function ActivityImpl({ actionFilter, filterMask }: ActivityImplProps) {
     let chips: JSX.Element[] = [];
 
     const fromDescription: { [key: string]: string } = {
-      "-15m": "15 minutes ago",
-      "-30m": "30 minutes ago",
-      "-1h": "1 hour ago",
-      "-24h": "24 hours ago",
-      "-7d": "7 days ago",
-      "-30d": "30 days ago",
+      '-15m': '15 minutes ago',
+      '-30m': '30 minutes ago',
+      '-1h': '1 hour ago',
+      '-24h': '24 hours ago',
+      '-7d': '7 days ago',
+      '-30d': '30 days ago',
     };
 
     if (audit.filter.from) {
       let description: string | undefined = fromDescription[audit.filter.from];
       if (!description) {
         let d = new Date(audit.filter.from);
-        description =
-          (d &&
-            (utc
-              ? formatUtcDate(d) + " (UTC)"
-              : formatLocalDate(d) + " (Local)")) ||
-          audit.filter.from;
+        description = (d && (utc ? formatUtcDate(d) + ' (UTC)' : formatLocalDate(d) + ' (Local)')) || audit.filter.from;
       }
-      chips.push(
-        <Chip
-          className={classes.chip}
-          key="from"
-          label={`From: ${description}`}
-        />
-      );
+      chips.push(<Chip className={classes.chip} key="from" label={`From: ${description}`} />);
       if (audit.filter.to) {
         let d = new Date(audit.filter.to);
         chips.push(
           <Chip
             className={classes.chip}
             key="to"
-            label={`To: ${utc ? formatUtcDate(d) : formatLocalDate(d)} ${
-              utc ? "(UTC)" : "(Local)"
-            }`}
+            label={`To: ${utc ? formatUtcDate(d) : formatLocalDate(d)} ${utc ? '(UTC)' : '(Local)'}`}
           />
         );
       }
     }
 
-    if (
-      audit.filter.resource &&
-      audit.filter.resource !== filterMask.resource
-    ) {
+    if (audit.filter.resource && audit.filter.resource !== filterMask.resource) {
       chips.push(
         <Chip
           className={classes.chip}
           key="resource"
           label={`Resource prefix: ${
-            filterMask.resource &&
-            audit.filter.resource.indexOf(filterMask.resource) === 0
+            filterMask.resource && audit.filter.resource.indexOf(filterMask.resource) === 0
               ? audit.filter.resource.substring(filterMask.resource.length)
               : audit.filter.resource
           }`}
@@ -288,7 +241,7 @@ function ActivityImpl({ actionFilter, filterMask }: ActivityImplProps) {
       );
     }
 
-    if (audit.filter.action && audit.filter.action !== "*") {
+    if (audit.filter.action && audit.filter.action !== '*') {
       chips.push(
         <Chip
           className={classes.chip}
@@ -336,7 +289,7 @@ function ActivityImpl({ actionFilter, filterMask }: ActivityImplProps) {
         defaultSortKey="timestamp"
         defaultSortOrder="desc"
         identityKey="id"
-        title="Activity"
+        title="Audit Activity"
         enableSelection={false}
         filterContent={
           <ActivityFilterContent
@@ -353,30 +306,19 @@ function ActivityImpl({ actionFilter, filterMask }: ActivityImplProps) {
   );
 }
 
-type ActivityProps = {
+type AuditProps = {
   filter: AuditFilter;
   actionFilter?: string[];
 };
 
-function Activity({ filter, actionFilter }: ActivityProps) {
-  const history = useHistory();
-  let hash: { [key: string]: string } = {};
+function AnalyticsAudit({ filter, actionFilter }: AuditProps) {
+  const [history] = useHashHistory('audit', {});
   let filterOverride = filter;
-  if (history.location.hash.length > 0) {
-    history.location.hash
-      .substring(1)
-      .split("&")
-      .forEach((e) => {
-        const [k, v] = e.split("=");
-        if (k !== undefined && k.length > 0 && v !== undefined) {
-          hash[k] = decodeURIComponent(v);
-        }
-      });
-    const { resource, action, from, to, issuerId, subject } = hash;
-    if (resource) {
-      filterOverride = { resource, action, from, to, issuerId, subject };
-    }
+  const { resource, action, from, to, issuerId, subject } = history;
+  if (resource) {
+    filterOverride = { resource, action, from, to, issuerId, subject };
   }
+
   if (!filterOverride.from && !filterOverride.to) {
     filterOverride.from = defaultFilterFrom;
   }
@@ -391,9 +333,10 @@ function Activity({ filter, actionFilter }: ActivityProps) {
   }
   return (
     <AuditProvider filter={filterOverride}>
-      <ActivityImpl filterMask={filter} actionFilter={actionFilter} />
+      <AnalyticsAuditImpl filterMask={filter} actionFilter={actionFilter} />
     </AuditProvider>
   );
 }
 
-export default Activity;
+export type IAnalyticsAuditProps = AuditProps;
+export { AnalyticsAudit };
