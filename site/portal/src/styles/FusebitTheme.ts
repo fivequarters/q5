445d6243cf1ef010cc1c7056488442ad8a67ee37
Yavor Georgiev <yavor@fusebit.io>
2020-05-22 17:41:10 -0700

    Adding system colors, updating HTTP code colors for portal monitoring

--- site/portal/src/styles/FusebitTheme.ts
+++ site/portal/src/styles/FusebitTheme.ts
@@ -1,49 +1,56 @@
-import { createMuiTheme } from "@material-ui/core/styles";
-import { FusebitColor } from "@5qtrs/fusebit-color";
+import { createMuiTheme } from '@material-ui/core/styles';
+import { FusebitColor } from '@5qtrs/fusebit-color';
 
 const FusebitTheme = {
   ...createMuiTheme({
     // Overrides of default MUI theme:
     typography: {
-      fontFamily: '"Nunito Sans", sans-serif'
+      fontFamily: '"Nunito Sans", sans-serif',
     },
     palette: {
       primary: {
-        main: FusebitColor.red
+        main: FusebitColor.red,
       },
       secondary: {
-        main: FusebitColor.orange
-      }
+        main: FusebitColor.orange,
+      },
+      error: {
+        main: FusebitColor.systemError,
+      },
+      warning: {
+        main: FusebitColor.systemWarning,
+      },
+      info: {
+        main: FusebitColor.systemNormal,
+      },
+      success: {
+        main: FusebitColor.systemSuccess,
+      },
     },
     overrides: {
       MuiTab: {
         wrapper: {
-          fontFamily: '"Poppins", sans-serif'
-        }
+          fontFamily: '"Poppins", sans-serif',
+        },
       },
       MuiButton: {
         root: {
-          borderRadius: "100px"
+          borderRadius: '100px',
         },
         label: {
-          fontFamily: '"Poppins", sans-serif'
-        }
-      }
-    }
+          fontFamily: '"Poppins", sans-serif',
+        },
+      },
+    },
   }),
   // Additional, Fusebit-specific theme properties  :
   fusebit: {
     colors: FusebitColor,
     profileSelector: {
       width: 240,
-      iconColors: [
-        FusebitColor.red,
-        FusebitColor.orange,
-        FusebitColor.cyan,
-        FusebitColor.gray
-      ]
-    }
-  }
+      iconColors: [FusebitColor.red, FusebitColor.orange, FusebitColor.cyan, FusebitColor.gray],
+    },
+  },
 };
 
 export default FusebitTheme;
