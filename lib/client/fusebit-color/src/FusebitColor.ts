1e603ca9fe7f48eda4b490b6a920e7c97d0e63d8
Yavor Georgiev <yavor@fusebit.io>
2020-05-22 17:41:10 -0700

    Adding system colors, updating HTTP code colors for portal monitoring

--- lib/client/fusebit-color/src/FusebitColor.ts
+++ lib/client/fusebit-color/src/FusebitColor.ts
@@ -9,6 +9,10 @@ const lightBlueHsl = [212, 100, 97];
 const grayHsl = [240, 21, 70];
 const blackHsl = [240, 88, 9];
 const whiteHsl = [0, 100, 100];
+const systemSuccessHsl = [123, 41, 45];
+const systemNormalHsl = [223, 81, 61];
+const systemWarningHsl = [38, 100, 50];
+const systemErrorHsl = [349, 100, 35];
 
 // ------------------
 // Internal Functions
@@ -30,6 +34,14 @@ function getHslValuesForColor(color: FusebitColor): number[] {
       return blackHsl.slice(0);
     case FusebitColor.white:
       return whiteHsl.slice(0);
+    case FusebitColor.systemSuccess:
+      return systemSuccessHsl.slice(0);
+    case FusebitColor.systemNormal:
+      return systemNormalHsl.slice(0);
+    case FusebitColor.systemWarning:
+      return systemWarningHsl.slice(0);
+    case FusebitColor.systemError:
+      return systemErrorHsl.slice(0);
     default:
       return [];
   }
@@ -47,6 +59,10 @@ export enum FusebitColor {
   gray = '#A1A1C2',
   black = '#03032D',
   white = '#FFFFFF',
+  systemSuccess = '#43A047',
+  systemNormal = '#4C79EC',
+  systemWarning = '#FFA000',
+  systemError = '#B00020',
 }
 
 // ------------------
@@ -84,24 +100,24 @@ export function opacity(color: FusebitColor, value: number): FusebitColor {
 
 type HttpCodeKeys = number | string;
 const httpCodeColorSet: { [code in HttpCodeKeys]: string } = {
-  ['2xx']: FusebitColor.cyan,
-  [200]: FusebitColor.cyan,
-  ['3xx']: FusebitColor.lightBlue,
-  [300]: darken(FusebitColor.cyan),
-  [301]: FusebitColor.lightBlue,
-  [304]: darken(FusebitColor.lightBlue),
-  [307]: darken(FusebitColor.lightBlue, 20),
-  ['4xx']: FusebitColor.orange,
-  [400]: darken(FusebitColor.orange),
-  [401]: darken(FusebitColor.orange, 20),
-  [403]: darken(FusebitColor.orange, 40),
-  [404]: FusebitColor.orange,
+  ['2xx']: FusebitColor.systemSuccess,
+  [200]: FusebitColor.systemSuccess,
+  ['3xx']: FusebitColor.systemNormal,
+  [300]: FusebitColor.systemNormal,
+  [301]: FusebitColor.systemNormal,
+  [304]: darken(FusebitColor.systemNormal),
+  [307]: darken(FusebitColor.systemNormal, 20),
+  ['4xx']: FusebitColor.systemWarning,
+  [400]: darken(FusebitColor.systemWarning),
+  [401]: darken(FusebitColor.systemWarning, 20),
+  [403]: darken(FusebitColor.systemWarning, 40),
+  [404]: FusebitColor.systemWarning,
   [410]: FusebitColor.gray,
-  ['5xx']: FusebitColor.red,
-  [500]: FusebitColor.red,
-  [501]: darken(FusebitColor.red),
-  [503]: darken(FusebitColor.red, 20),
-  [550]: darken(FusebitColor.red, 30),
+  ['5xx']: FusebitColor.systemError,
+  [500]: FusebitColor.systemError,
+  [501]: darken(FusebitColor.systemError),
+  [503]: darken(FusebitColor.systemError, 20),
+  [550]: darken(FusebitColor.systemError, 30),
 };
 
 export function httpCodeColorMap(code: number | string) {
