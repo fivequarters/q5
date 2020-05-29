50f6bf60e454a726e7ba4755cc14f4264acc2f11
Yavor Georgiev <yavor@fusebit.io>
2020-05-28 19:28:01 -0700

    Switching portal error/warnings to use system colors

--- site/portal/src/components/WarningCard.tsx
+++ site/portal/src/components/WarningCard.tsx
@@ -1,46 +1,43 @@
-import Paper from "@material-ui/core/Paper";
-import { makeStyles } from "@material-ui/core/styles";
-import WarningIcon from "@material-ui/icons/Warning";
-import React from "react";
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import WarningIcon from '@material-ui/icons/Warning';
+import React from 'react';
 
 const useStyles = makeStyles((theme: any) => ({
   root: (props: any) => ({
     paddingRight: props.padding ? theme.spacing(2) : 0,
-    paddingLeft: props.padding ? theme.spacing(2) : 0
+    paddingLeft: props.padding ? theme.spacing(2) : 0,
   }),
   card: (props: any) => ({
     minWidth: 200,
-    width: "100%",
+    width: '100%',
     marginTop: theme.spacing(2),
     marginBottom: theme.spacing(2),
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "stretch",
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
     // minHeight: 114,
     // height: 114,
-    backgroundColor:
-      props.color === "primary"
-        ? theme.palette.primary.main
-        : theme.palette.secondary.main
+    backgroundColor: props.color === 'error' ? theme.palette.error.main : theme.palette.warning.main,
   }),
-  cardIcon: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+  cardIcon: (props: any) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
     width: 70,
     minWidth: 70,
     fontSize: 18,
-    color: "white"
-  },
-  cardContent: {
-    width: "100%",
+    color: props.color === 'error' ? theme.palette.error.contrastText : theme.palette.warning.contrastText,
+  }),
+  cardContent: (props: any) => ({
+    width: '100%',
     padding: theme.spacing(1),
     paddingRight: theme.spacing(3),
     paddingLeft: 0,
-    display: "inline-block",
-    alignItems: "center",
-    color: "white"
-  }
+    display: 'inline-block',
+    alignItems: 'center',
+    color: props.color === 'error' ? theme.palette.error.contrastText : theme.palette.warning.contrastText,
+  }),
 }));
 
 function WarningCard({ icon, padding, children, color, ...rest }: any) {
