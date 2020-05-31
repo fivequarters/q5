673aefae2bf054d90c6542761bd411fc85ed4156
Yavor Georgiev <yavor@fusebit.io>
2020-05-30 20:45:33 -0700

    Making all selects in portal consistent, moving to consistnet use of filled inputs for dialogs and outlined inputs for regular pages

--- site/portal/src/components/PublicKeyAcquisitionSelector.tsx
+++ site/portal/src/components/PublicKeyAcquisitionSelector.tsx
@@ -1,42 +1,36 @@
-import FormControl from "@material-ui/core/FormControl";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import Select from "@material-ui/core/Select";
-import VpnKeyIcon from "@material-ui/icons/VpnKey";
-import React from "react";
-import InputWithIcon from "./InputWithIcon";
-import { modifyIssuer, useIssuer } from "./IssuerProvider";
+import MenuItem from '@material-ui/core/MenuItem';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import React from 'react';
+import InputWithIcon from './InputWithIcon';
+import { modifyIssuer, useIssuer } from './IssuerProvider';
+import { TextField } from '@material-ui/core';
 
-function PublicKeyAcquisitionSelector() {
+function PublicKeyAcquisitionSelector({ variant }: any) {
   const [issuer, setIssuer] = useIssuer();
 
-  const handlePublicKeyAcquisitionChange = (
-    event: React.ChangeEvent<{ value: unknown }>
-  ) => {
-    if (issuer.status === "ready") {
-      issuer.modified.publicKeyAcquisition =
-        event.target.value === "pki" ? "pki" : "jwks";
+  const handlePublicKeyAcquisitionChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    if (issuer.status === 'ready') {
+      issuer.modified.publicKeyAcquisition = event.target.value === 'pki' ? 'pki' : 'jwks';
       modifyIssuer(issuer, setIssuer, { ...issuer.modified });
     }
   };
 
-  if (issuer.status === "ready" || issuer.status === "updating") {
+  if (issuer.status === 'ready' || issuer.status === 'updating') {
     return (
       <InputWithIcon icon={<VpnKeyIcon />}>
-        <FormControl fullWidth>
-          <InputLabel htmlFor="publicKeyAcquisition">
-            Public Key Acquisition
-          </InputLabel>
-          <Select
-            id="publicKeyAcquisition"
-            value={issuer.modified.publicKeyAcquisition}
-            onChange={handlePublicKeyAcquisitionChange}
-            disabled={issuer.status === "updating"}
-          >
-            <MenuItem value={"pki"}>Stored Public Key</MenuItem>
-            <MenuItem value={"jwks"}>JWKS Endpoint</MenuItem>
-          </Select>
-        </FormControl>
+        <TextField
+          id="publicKeyAcquisition"
+          select
+          fullWidth
+          label="Public Key Acquisition"
+          value={issuer.modified.publicKeyAcquisition}
+          onChange={handlePublicKeyAcquisitionChange}
+          disabled={issuer.status === 'updating'}
+          variant={variant || 'outlined'}
+        >
+          <MenuItem value={'pki'}>Stored Public Key</MenuItem>
+          <MenuItem value={'jwks'}>JWKS Endpoint</MenuItem>
+        </TextField>
       </InputWithIcon>
     );
   }
