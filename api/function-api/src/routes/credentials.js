373a6c0fb7bad3efaeeb865a4c1ba37821ecb2d5
Benn Bollay <bb.github@magitech.org>
2020-06-01 11:28:33 -0700

    .

--- api/function-api/src/routes/credentials.js
+++ api/function-api/src/routes/credentials.js
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk');
 
-const credentialRefreshWindow = 10 * 60; // 10 minutes left
-const defaultCredentialDuration = 12 * 60 * 60; // 12 hours
+const credentialRefreshWindowSecs = 60; // 10 * 60; // 10 minutes left
+const defaultCredentialDurationSecs = 5 * 60; // 12 * 60 * 60; // 12 hours
 
 // Popualate the cache with the environment-supplied variables, if any.
 let credentialCache = {
@@ -12,47 +12,55 @@ let credentialCache = {
   sessionToken: process.env.AWS_SESSION_TOKEN || '',
 
   // If the credentials are provided via the environment (i.e. for testing), don't refresh.
-  expiration: process.env.AWS_SESSION_TOKEN ? Date.now() + defaultCredentialDuration * 1000 : 0,
+  expiration: process.env.AWS_SESSION_TOKEN ? Date.now() + defaultCredentialDurationSecs * 1000 : 0,
 };
+
+let refreshInProgress = undefined;
 
 // Query the metadata server on an EC2 instance to acquire new credentials
 const refreshCredentials = async () => {
-  console.log(`CRED: Refreshing (previous expiration ${new Date(credentialCache.expiration).toISOString()})`);
+  if (refreshInProgress) {
+    return await refreshInProgress;
+  }
 
-  await new Promise((resolve, reject) => {
-    const credentials = new AWS.EC2MetadataCredentials({
-      httpOptions: { timeout: 5000 }, // 5 second timeout
-      maxRetries: 10, // retry 10 times
-      retryDelayOptions: { base: 200 }, // see AWS.Config for information
-    });
-
-    credentials.refresh(err => {
-      if (err) {
-        console.log('CRED: Failure to acquire AWS credentials:', err);
-        return process.exit(1);
-      }
-
-      let creds = credentials.metadata;
-      credentialCache = {
-        account: process.env.AWS_ACCOUNT,
-        accessKeyId: creds.AccessKeyId,
-        secretAccessKey: creds.SecretAccessKey,
-        sessionToken: creds.Token,
-        expiration: Date.parse(creds.Expiration),
-      };
-      console.log(`CRED: Refreshed (new expiration ${new Date(credentialCache.expiration).toISOString()})`);
-      resolve();
-    });
+  const credentials = new AWS.EC2MetadataCredentials({
+    httpOptions: { timeout: 5000 }, // 5 second timeout
+    maxRetries: 10, // retry 10 times
+    retryDelayOptions: { base: 200 }, // see AWS.Config for information
   });
+
+  try {
+    console.log(`CRED: Refreshing (previous expiration ${new Date(credentialCache.expiration).toISOString()})`);
+    refreshInProgress = credentials.refreshPromise();
+    await refreshInProgress;
+  } catch (e) {
+    console.log('CRED: Failure to acquire AWS credentials:', e);
+    return process.exit(1);
+  }
+
+  let creds = credentials.metadata;
+  credentialCache = {
+    account: process.env.AWS_ACCOUNT,
+    accessKeyId: creds.AccessKeyId,
+    secretAccessKey: creds.SecretAccessKey,
+    sessionToken: creds.Token,
+    expiration: Date.now() + 60 * 5 * 1000, // XXX Date.parse(creds.Expiration),
+  };
+
+  refreshInProgress = undefined;
+  console.log(`CRED: Refreshed (new expiration ${new Date(credentialCache.expiration).toISOString()})`);
 };
 
-// Return the current credentials with at least credentialRefreshWindow seconds of validity
+// Return the current credentials with at least credentialRefreshWindowSecs seconds of validity
 const getAWSCredentials = async (wait = true) => {
-  if (credentialCache.expiration < Date.now() + credentialRefreshWindow) {
+  if (credentialCache.expiration < Date.now() + credentialRefreshWindowSecs * 1000) {
     if (wait) {
       await refreshCredentials();
     } else {
-      return undefined;
+      refreshCredentials();
+      if (credentialCache.expiration < Date.now()) {
+        return undefined;
+      }
     }
   }
 
