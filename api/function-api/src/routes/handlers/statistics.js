6e93a8f862ac4fee01cedc3ea4f588eb5c9b88c5
Benn Bollay <bb.github@magitech.org>
2020-06-06 21:16:57 -0700

    Address two non-breaking exceptions when ES is not configured

--- api/function-api/src/routes/handlers/statistics.js
+++ api/function-api/src/routes/handlers/statistics.js
@@ -42,9 +42,9 @@ const onStartup = async () => {
   }
 
   console.log(
-    `ES: Elastic Search configuration: ${process.env.ES_USER}:${process.env.ES_PASSWORD.length > 0 ? '*' : 'X'}@${
-      process.env.ES_HOST
-    }`
+    `ES: Elastic Search configuration: ${process.env.ES_USER}:${
+      process.env.ES_PASSWORD && process.env.ES_PASSWORD.length > 0 ? '*' : 'X'
+    }@${process.env.ES_HOST}`
   );
 
   // Utility functions for authenticating to the ElasticSearch service on the right target
@@ -114,7 +114,7 @@ const onStartup = async () => {
     console.log('ES: Using username/password authentication');
     getRegion = () => '';
     authRequest = (opts, cred) => (opts.auth = `${process.env.ES_USER}:${process.env.ES_PASSWORD}`);
-  } else {
+  } else if (process.env.ES_HOST) {
     console.log('ES: Using IAM authentication');
     getRegion = () => {
       return process.env.ES_HOST.match(/^([^\.]+)\.?([^\.]*)\.?([^\.]*)\.amazonaws\.com$/)[2];
