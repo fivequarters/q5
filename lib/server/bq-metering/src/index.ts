7506ea2f9b8c0fbf01a81a2b9bb2881cd1b9c036
Benn Bollay <bb.github@magitech.org>
2020-12-01 11:16:42 -0800

    Remove unnecessary log messages (#811)
    * Remove SUCCESS log messages from BQ
    * Remove extraneous real-time logging information message

--- lib/server/bq-metering/src/index.ts
+++ lib/server/bq-metering/src/index.ts
@@ -106,9 +106,6 @@ function flushApiMetering() {
   bq.dataset('dwh')
     .table('api_call')
     .insert(tmp, { raw: true })
-    .then(async () => {
-      console.log(`SUCCESS inserting ${tmp.length} records to dwh.api_call table in Big Query`);
-    })
     .catch((e: any) => {
       console.log(`ERROR inserting ${tmp.length} records to dwh.api_call table in Big Query`, e.message);
       console.log('ERROR[0]', e.errors ? JSON.stringify(e.errors[0], null, 2) : 'NA');
@@ -143,9 +140,6 @@ function generateExecutionCounters() {
     bq.dataset('dwh')
       .table('execution')
       .insert(bqInsertPayload, { raw: true })
-      .then(async () => {
-        console.log(`SUCCESS inserting ${bqInsertPayload.length} records to dwh.execution table in Big Query`);
-      })
       .catch((e: any) => {
         console.log(`ERROR inserting ${bqInsertPayload.length} records to dwh.execution table in Big Query`, e.message);
         console.log('ERROR[0]', e.errors ? JSON.stringify(e.errors[0], null, 2) : 'NA');
