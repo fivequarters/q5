21e1c47265b288b1ad4f769dee82ccab0056fb52
Benn Bollay <bb.github@magitech.org>
2020-12-01 09:24:09 -0800

    Fully remove the 'then' clause'

--- lib/server/bq-metering/src/index.ts
+++ lib/server/bq-metering/src/index.ts
@@ -106,7 +106,6 @@ function flushApiMetering() {
   bq.dataset('dwh')
     .table('api_call')
     .insert(tmp, { raw: true })
-    .then(async () => {})
     .catch((e: any) => {
       console.log(`ERROR inserting ${tmp.length} records to dwh.api_call table in Big Query`, e.message);
       console.log('ERROR[0]', e.errors ? JSON.stringify(e.errors[0], null, 2) : 'NA');
@@ -141,7 +140,6 @@ function generateExecutionCounters() {
     bq.dataset('dwh')
       .table('execution')
       .insert(bqInsertPayload, { raw: true })
-      .then(async () => {})
       .catch((e: any) => {
         console.log(`ERROR inserting ${bqInsertPayload.length} records to dwh.execution table in Big Query`, e.message);
         console.log('ERROR[0]', e.errors ? JSON.stringify(e.errors[0], null, 2) : 'NA');
