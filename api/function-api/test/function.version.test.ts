bb6b48672827cc41f84843fa0c0d57b1b06efe4b
Benn Bollay <bb.github@magitech.org>
2021-01-12 22:20:06 -0800

    Add a test for $LATEST

--- api/function-api/test/function.version.test.ts
+++ api/function-api/test/function.version.test.ts
@@ -21,22 +21,28 @@ const makeFunction = (version: number) => {
   };
 };
 
-const demoteFunction = async (targetVersion: number) => {
+const demoteFunction = async (targetVersion?: number) => {
   const functionKey = `${account.accountId}/${account.subscriptionId}/${boundaryId}/${function1Id}`;
 
+  const params: DynamoDB.UpdateItemInput = {
+    TableName: keyValueTableName,
+    ExpressionAttributeNames: { '#FV': 'environment.function' },
+    Key: {
+      category: { S: 'function-tags-function' },
+      key: { S: functionKey },
+    },
+    UpdateExpression: 'SET #FV = :ver',
+  };
+
+  if (targetVersion !== undefined) {
+    params.ExpressionAttributeValues = { ':ver': { N: `${targetVersion}` } };
+    params.UpdateExpression = 'SET #FV = :ver';
+  } else {
+    params.UpdateExpression = 'REMOVE #FV';
+  }
+
   // Demote the version in the database, to force an older version to get called.
-  await dynamo
-    .updateItem({
-      TableName: keyValueTableName,
-      ExpressionAttributeNames: { '#FV': 'environment.function' },
-      ExpressionAttributeValues: { ':ver': { N: `${targetVersion}` } },
-      Key: {
-        category: { S: 'function-tags-function' },
-        key: { S: functionKey },
-      },
-      UpdateExpression: 'SET #FV = :ver',
-    })
-    .promise();
+  await dynamo.updateItem(params).promise();
 };
 
 describe('function.versions', () => {
@@ -72,6 +78,30 @@ describe('function.versions', () => {
     await demoteFunction(1);
     response = await callFunction('', location);
     expect(response.data).toBe(1);
+  }, 120000);
+
+  test('missing metadata executes $LATEST', async () => {
+    disableFunctionUsageRestriction();
+
+    // Bump the function a few times
+    let response = await putFunction(account, boundaryId, function1Id, makeFunction(1));
+    expect(response).toBeHttp({ statusCode: 200 });
+    response = await putFunction(account, boundaryId, function1Id, makeFunction(2));
+    expect(response).toBeHttp({ statusCode: 200 });
+    const location = response.data.location;
+
+    response = await callFunction('', location);
+    expect(response.data).toBe(2);
+
+    // Validate that the expected function gets executed
+    await demoteFunction(1);
+    response = await callFunction('', location);
+    expect(response.data).toBe(1);
+
+    // Now remove the entry and validate it running $LATEST
+    await demoteFunction(undefined);
+    response = await callFunction('', location);
+    expect(response.data).toBe(2);
   }, 120000);
 
   test('old function versions removed', async () => {
