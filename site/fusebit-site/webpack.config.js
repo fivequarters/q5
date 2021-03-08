86a505cd1a9b999c51aa97345d4b6599b6061b6a
Jacob Haller-Roby <jacob@Jacobs-MacBook-Pro.local>
2021-03-08 11:03:51 -0800

    Updating local webpack config

--- site/fusebit-site/webpack.config.js
+++ site/fusebit-site/webpack.config.js
@@ -1,4 +1,11 @@
 const { webpackDev } = require('@5qtrs/tool-config');
 const packageJson = require('./package.json');
+const html = require('./html.json');
+
 const options = { html: { default: { title: 'Fusebit' } } };
+
+for (const path in html) {
+  options.html[path] = { title: path.title };
+}
+
 module.exports = webpackDev(packageJson, options);
