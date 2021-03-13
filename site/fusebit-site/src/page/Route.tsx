65bcd90002260444b4c7a5345b42ade2e7176333
Jacob Haller-Roby <jacob@Jacobs-MacBook-Pro.local>
2021-03-12 16:07:16 -0800

    Adding htmlHeadSnippet to webpack template process

--- site/fusebit-site/src/page/Route.tsx
+++ site/fusebit-site/src/page/Route.tsx
@@ -6,15 +6,14 @@ import _html from '../config/html.json';
 
 const html: {
   [key: string]: {
-    title?: string,
-    description?: string
-  },
+    title?: string;
+    description?: string;
+  };
   default: {
-    title: string,
-    description: string
-  }
+    title: string;
+    description: string;
+  };
 } = _html;
-
 
 export const Route = (props: RouteProps) => {
   let title: string = html.default.title;
@@ -24,16 +23,17 @@ export const Route = (props: RouteProps) => {
     description = html[props.path].description || description;
   }
 
-  return <React.Fragment>
-    <Helmet>
-      <script src="https://www.googleoptimize.com/optimize.js?id=OPT-KBDMH9D"></script> 
-      <title>{title}</title>
-      <meta name='og:title' content={title} />
-      <meta name='twitter:title' content={title} />
-      <meta name='description' content={description} />
-      <meta name='og:description' content={description} />
-      <meta name='twitter:description' content={description} />
-    </Helmet>
-    <ReactRouterRoute {...props}/>
-  </React.Fragment>;
+  return (
+    <React.Fragment>
+      <Helmet>
+        <title>{title}</title>
+        <meta name="og:title" content={title} />
+        <meta name="twitter:title" content={title} />
+        <meta name="description" content={description} />
+        <meta name="og:description" content={description} />
+        <meta name="twitter:description" content={description} />
+      </Helmet>
+      <ReactRouterRoute {...props} />
+    </React.Fragment>
+  );
 };
