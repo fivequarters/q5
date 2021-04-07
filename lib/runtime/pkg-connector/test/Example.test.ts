ea0a4b5211f612a629dad39e294d3f8c09927905
Benn Bollay <bb.github@magitech.org>
2021-04-06 20:45:11 -0700

    .

--- lib/runtime/pkg-connector/test/Example.test.ts
+++ lib/runtime/pkg-connector/test/Example.test.ts
@@ -2,8 +2,13 @@ import Router, { Manager, Context } from '../src';
 
 describe('Example', () => {
   it('Example implementation', async () => {
-    // In the vendor's code:
+    /////////////////////////////////////////////////////////////////////////
+    // Vendor's Code: @vendor/slack7/index.js
+    //
+    // Implied:
+    //   import Router from '@fusebit/slack-connector';
     const router = new Router();
+
     router.get('/sendMessage', async (ctx: Context) => {
       ctx.body = 'Message Sent';
     });
@@ -12,7 +17,15 @@ describe('Example', () => {
       ctx.status = 418;
     });
 
-    // In a @fusebit/slack-connector
+    router.on('newUserCreated', async (ctx: Context) => {
+      // do something in the database
+      //
+      // Return 200 (optional; default return value is 200 if unspecified).
+      ctx.status = 200;
+    });
+
+    /////////////////////////////////////////////////////////////////////////
+    // Owned by Fusebit: @fusebit/slack-connector/index.js
     class SlackConnector extends Manager {
       public addHttpRoutes() {
         this.router.get('/health', async (ctx, next) => {
@@ -21,21 +34,37 @@ describe('Example', () => {
       }
     }
 
-    // In the top level index.js of the Fusebit function
+    /////////////////////////////////////////////////////////////////////////
+    // Created by function-api: Fusebit Function nodejs['index.js']:
+    //
+    // Implied:
+    //   const router = require('@vendor/slack7');
     const manager = new SlackConnector();
     manager.setup(router, undefined);
 
-    // In the handler in index.js for fusebit events
+    /////////////////////////////////////////////////////////////////////////
+    // Test Calls
+
+    // A request for 'GET /sendMessage':
     let fusebitEventContext: any;
 
     fusebitEventContext = { body: {}, headers: {}, method: 'GET', path: '/sendMessage' };
     let result = await manager.handle(fusebitEventContext);
     expect(result.body).toBe('Message Sent');
 
+    // A request for 'GET /health':
     fusebitEventContext = { body: {}, headers: {}, method: 'GET', path: '/health' };
     result = await manager.handle(fusebitEventContext);
     expect(result.statusCode).toBe(418);
 
+    // The CRON triggers:
+    //
+    // Implied, in fusebit.json:
+    //   cron: {
+    //     default: {
+    //       schedule: '* * * * *'
+    //     }
+    //   }
     fusebitEventContext = { body: {}, headers: {}, method: 'CRON', path: 'default' };
     result = await manager.handle(fusebitEventContext);
     expect(result.statusCode).toBe(418);
