5b66f6cdbdeeb0c229f15ea0499827f9d4689514
Benn Bollay <bb.github@magitech.org>
2021-04-16 20:29:42 -0700

    Update some comments

--- lib/runtime/pkg-oauth-integration/src/OAuthIntegration.ts
+++ lib/runtime/pkg-oauth-integration/src/OAuthIntegration.ts
@@ -2,6 +2,10 @@ import superagent from 'superagent';
 
 import { Context, IConnectorConfig } from '@fusebit-int/pkg-manager';
 
+// An example class that pairs with the OAuthConnector.  Many such classes may pair with the OAuthConnector
+// (for those that are fairly generic in their OAuth usage).  There's no expectation nor need for them to
+// derive from this particular instance, though by doing so they can super.instantiate() before creating their
+// desired SDK specialization.
 export default class OAuthIntegration {
   public config: IConnectorConfig;
   constructor(cfg: IConnectorConfig) {
@@ -9,14 +13,15 @@ export default class OAuthIntegration {
   }
 
   // The ctx is needed so that the integration can hook out auth tokens from the request.
+  //
+  // Normally, this function would return an instantiated SDK object populated and enriched as appropriate.
+  // For now, just return the accessToken for the caller to do with as they please.
   public async instantiate(ctx: Context, lookupKey: string) {
-    // Send request to authority/token passing in the lookupKey - XXX accessToken not actually plumbed into
-    // the context yet.
+    // Send request to authority/token passing in the lookupKey
     const tokenResponse = await superagent
       .get(`${this.config.config.authority}/${lookupKey}/token`)
       .set('Authorization', `Bearer ${ctx.fusebit.functionAccessToken}`);
 
-    console.log(`OAuthIntegration::instantiate - ${tokenResponse.status} - ${JSON.stringify(tokenResponse.body)}`);
     // Take the responding token, put it into the object below.
     return {
       accessToken: tokenResponse.body.access_token,
