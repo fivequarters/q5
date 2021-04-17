7261c73aed33ad89f2c6e3540c183fcb7ee81083
Benn Bollay <bb.github@magitech.org>
2021-04-16 20:24:06 -0700

    Remove unused pkg-integration

--- lib/runtime/pkg-integration/src/FusebitIntegration.ts
+++
@@ -1,6 +0,0 @@
-import { FusebitManager } from '@fusebit-int/pkg-manager';
-
-class FusebitIntegration extends FusebitManager {
-  // itemize out all of the various endpoints that an integration will need to deal with from an
-  // infrastructure perspective.
-}
--- lib/runtime/pkg-integration/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as NewClass } from './NewClass';
