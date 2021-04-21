094d5551739950842c1e325f95f8147233f9a36c
Benn Bollay <bb.github@magitech.org>
2021-04-21 14:51:10 -0700

    Update 1_slack_message_response.ts

--- lib/package/comps/src/1_slack_message_response.ts
+++ lib/package/comps/src/1_slack_message_response.ts
@@ -6,9 +6,17 @@ const router = new Fusebit.Router();
  * Send a "Hello World!" message to the user configured as 'tenantId'.
  */
 router.get('/hello/:tenantId', async (ctx) => {
-  const slack = await Fusebit.Connectors.GetClient('slack', ctx.params.tenantId);
+  /*
+  const fusebit = new Fusebit(ctx);
 
-  await slack.sendMessage(`Hello User ${ctx.params.tenantId}`);
+  const instance = await ctx.Fusebit.Integrations.GetInstance(ctx.params.tenantId);
+  const identity = await ctx.Fusebit.Connectors.GetIdentity('slack', instance);
+  const slack = await ctx.Fusebit.Connectors.GetSdk('slack', identity);
+  */
+  const slack = await ctx.Fusebit.Connectors.slack1.GetSdk(ctx.params.tenantId);
+  const config = await ctx.Fusebit.GetInstanceConfig(ctx);
+
+  await slack.bot.chat.postMessage({ text: `Hello User!`, channel: config.slack_channel_name });
 
   ctx.body = { status: 'success' };
 });
@@ -16,12 +24,18 @@ router.get('/hello/:tenantId', async (ctx) => {
 /*
  * A user sent a message to the bot in Slack, which has been already mapped to a particular tenantId.
  */
-router.on('slack.message', async (ctx) => {
-  const contoso = await Fusebit.Connectors.GetClient('contoso', ctx.params.tenantId);
+router.on('slack1.message', async (ctx) => {
+  const contoso = await ctx.Fusebit.Connectors.GetClient('contoso', ctx.body.tags.tenantId);
+  const config = await ctx.Fusebit.GetInstanceConfig(ctx);
 
-  await contoso.notifyOnSlackMessage(ctx.body);
+  await contoso.notifyOnSlackMessage(ctx.body.event);
 
   ctx.body = { status: 'success' };
 });
 
+/*
+Possibly in the connector.
+router.on('slack.unknownUser', async (ctx) => {
+});
+*/
 export default router;
