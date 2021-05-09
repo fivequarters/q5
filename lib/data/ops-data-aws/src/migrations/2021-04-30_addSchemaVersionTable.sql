cbbc47833b57dfa5ff8c08a53c24053b28afd0eb
jacob <4974971+jscotroby@users.noreply.github.com>
2021-05-09 00:10:46 -0700

    Re-order of migrations to handle schemaVersion change for deployments at any stage

--- lib/data/ops-data-aws/src/migrations/2021-04-30_addSchemaVersionTable.sql
+++ lib/data/ops-data-aws/src/migrations/2021-04-30_addSchemaVersionTable.sql
@@ -9,3 +9,21 @@ create table schemaVersion (
 revoke delete, truncate on schemaVersion from public;
 
 insert into schemaVersion values (true, -1);
+-- recreating schemaVersion table to hold history of migration dates
+-- this is pasted both here and in a future migration to facilitate
+--  the migration process for deployments at either stage
+alter table schemaVersion rename to schemaVersionRotateOut;
+
+create table schemaVersion (
+    id SERIAL PRIMARY KEY NOT NULL,
+    version bigint unique,
+    fuse_ops_version varchar(30),
+    ran_at timestamptz
+);
+
+insert into schemaVersion (version, ran_at)
+SELECT generate_series(-1,version), now() from  schemaVersionRotateOut;
+
+revoke delete, truncate on schemaVersion from public;
+
+drop table schemaVersionRotateOut;
