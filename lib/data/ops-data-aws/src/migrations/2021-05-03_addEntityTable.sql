1b3e84da1d92ddf400b50910f3b94bec54b529ac
Benn Bollay <bb.github@magitech.org>
2021-05-10 23:48:46 -0700

    Database style and functional tweaks
    Style changes and a couple of bug fixes.

--- lib/data/ops-data-aws/src/migrations/2021-05-03_addEntityTable.sql
+++ lib/data/ops-data-aws/src/migrations/2021-05-03_addEntityTable.sql
@@ -2,28 +2,28 @@
 CREATE TYPE entity_type AS ENUM ('integration', 'connector', 'storage', 'operation');
 
 -- create new table
-create table entity (
+CREATE TABLE entity (
     id SERIAL PRIMARY KEY NOT NULL,
-    entityType entity_type not null,
-    accountId varchar not null,
-    subscriptionId varchar not null,
-    entityId varchar not null,
-    version bigint not null,
-    data jsonb not null,
-    tags jsonb not null,
-    expires timestamptz
+    entityType entity_type NOT NULL,
+    accountId VARCHAR NOT NULL,
+    subscriptionId VARCHAR NOT NULL,
+    entityId VARCHAR NOT NULL,
+    version BIGINT NOT NULL,
+    data JSONB NOT NULL,
+    tags JSONB NOT NULL,
+    expires TIMESTAMPTZ
 );
 -- Add constraints
 CREATE UNIQUE INDEX entity_pri_key ON entity (entityType, accountId, subscriptionId, entityId);
-create index entity_tags_idx on entity using gin (tags);
-create index entity_expires_idx on entity (expires);
+CREATE INDEX entity_tags_idx ON entity USING gin (tags);
+CREATE INDEX entity_expires_idx ON entity (expires);
 
-create function version_conflict() returns trigger as $$
-begin
-	raise exception 'version_conflict' using errcode = '22000';
-end;
-$$ language plpgsql;
+CREATE FUNCTION version_conflict() RETURNS TRIGGER AS $$
+BEGIN
+	RAISE EXCEPTION 'version_conflict' USING errcode = '22000';
+END;
+$$ LANGUAGE PLPGSQL;
 
-create trigger version_check before update of version on entity for each row
-when (NEW.version != (OLD.version + 1))
-execute procedure version_conflict();
+CREATE TRIGGER version_check BEFORE UPDATE OF version ON entity FOR EACH ROW
+  WHEN (NEW.version != (OLD.version + 1))
+  EXECUTE PROCEDURE version_conflict();
