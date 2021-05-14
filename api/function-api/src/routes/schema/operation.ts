afd4067ceba7d236164062759beca421e5ec6363
jacob <4974971+jscotroby@users.noreply.github.com>
2021-05-14 08:51:50 -0700

    little cleanup

--- api/function-api/src/routes/schema/operation.ts
+++ api/function-api/src/routes/schema/operation.ts
@@ -1,17 +1,17 @@
-// import express from 'express';
-//
-// import * as common from '../middleware/common';
-//
-// import * as operation from '../handlers/operation';
-//
-// const router = express.Router({ mergeParams: true });
-//
-// router.route('/').options(common.cors()).post(common.management({}), operation.post);
-//
-// router
-//   .route('/:operationId')
-//   .options(common.cors())
-//   .get(common.management({}), operation.get)
-//   .put(common.management({}), operation.put);
-//
-// export { router as default };
+import express from 'express';
+
+import * as common from '../middleware/common';
+
+import * as operation from '../handlers/operation';
+
+const router = express.Router({ mergeParams: true });
+
+router.route('/').options(common.cors()).post(common.management({}), operation.post);
+
+router
+  .route('/:operationId')
+  .options(common.cors())
+  .get(common.management({}), operation.get)
+  .put(common.management({}), operation.put);
+
+export default router;
