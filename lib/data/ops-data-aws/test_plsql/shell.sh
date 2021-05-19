3745ca1bcd4b250a2a557f2ca04e7010015fa6d8
Benn Bollay <bb.github@magitech.org>
2021-05-18 19:32:59 -0700

    Small tooling update (NFC)

--- lib/data/ops-data-aws/test_plsql/shell.sh
+++ lib/data/ops-data-aws/test_plsql/shell.sh
@@ -1,3 +1,6 @@
-aws --profile=fusestage --region us-west-1 rds-data execute-statement --database fusebit \
-  --secret-arn arn:aws:secretsmanager:us-west-1:749775346857:secret:rds-db-credentials/fusebit-db-secret-benn-3a0aa0a9ee63a1de9087-qmVaRo \
-  --resource-arn arn:aws:rds:us-west-1:749775346857:cluster:fusebit-db-benn --sql "$*"
+#!/bin/bash
+
+source env.${PROFILE}
+aws --profile=${AWS_PROFILE} --region ${REGION} rds-data execute-statement --database fusebit \
+  --secret-arn  ${SECRET_ARN} \
+  --resource-arn ${DATABASE_ARN} --sql "$*"
