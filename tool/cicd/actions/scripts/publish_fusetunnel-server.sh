e487402e36f3010c4ce612a2b4a1be28ec3405c1
Zhixiang Zhao <matthew@mail.matthew-cloud.com>
2021-05-20 12:16:45 -0700

    proper ver

--- tool/cicd/actions/scripts/publish_fusetunnel-server.sh
+++ tool/cicd/actions/scripts/publish_fusetunnel-server.sh
@@ -11,14 +11,14 @@ git clone https://github.com/fusebit/tunnel-server
 
 # -- Optional Parameters --
 AWS_PROFILE=${AWS_PROFILE:=default}
-VERSION=${VERSION_FUSEBIT_CLI:=`jq -r '.version' ./tunnel-server/package.json`}
+TUNNELSERVER_VERSION=${VERSION_FUSEBIT_CLI:=`jq -r '.version' ./tunnel-server/package.json`}
 
 echoerr "Deploying to npm (ignoring error on republish of same version)"
 cd tunnel-server/
 npm publish 1>&2 --access public || true
 
 echoerr "Testing installation"
-npm install -g @fusebit/tunnel-server@${VERSION} 1>&2
+npm install -g @fusebit/tunnel-server@${TUNNELSERVER_VERSION} 1>&2
 
 echoerr "Completed successfully:"
-echo { \"version\": \"${VERSION}\" }
+echo { \"version\": \"${TUNNELSERVER_VERSION}\" }
