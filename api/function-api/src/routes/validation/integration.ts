46d840aa3f5276502bf93245bfac6bc1942c0b61
jacob <4974971+jscotroby@users.noreply.github.com>
2021-07-01 16:09:45 -0700

    integration validation fix

--- api/function-api/src/routes/validation/integration.ts
+++ api/function-api/src/routes/validation/integration.ts
@@ -6,30 +6,28 @@ import * as Session from './session';
 import * as Common from './common';
 
 const Data = Joi.alternatives().try(
-  Joi.object()
-    .keys({
-      handler: Joi.string().required(),
-      configuration: Joi.object()
-        .keys({
-          connectors: Joi.object()
-            .pattern(
-              Common.entityId,
-              Joi.object().keys({
-                package: Common.npmPackageName,
-                connector: Common.entityId,
-              })
-            )
-            .max(10), // Arbitrary
-          creation: Joi.object().keys({
-            tags: Common.tags,
-            steps: Joi.array().items(Session.Step).max(10), // Arbitrary
-            autoStep: Joi.boolean().optional().default(false),
-          }),
-        })
-        .required(),
-      files: EntityCommon.Files.required(),
-    })
-    .unknown(true),
+  Joi.object().keys({
+    handler: Joi.string().required(),
+    configuration: Joi.object()
+      .keys({
+        connectors: Joi.object()
+          .pattern(
+            Common.entityId,
+            Joi.object().keys({
+              package: Common.npmPackageName,
+              connector: Common.entityId,
+            })
+          )
+          .max(10), // Arbitrary
+        creation: Joi.object().keys({
+          tags: Common.tags,
+          steps: Joi.array().items(Session.Step).max(10), // Arbitrary
+          autoStep: Joi.boolean().optional().default(false),
+        }),
+      })
+      .required(),
+    files: EntityCommon.Files.required(),
+  }),
   Joi.object().keys({})
 );
 
