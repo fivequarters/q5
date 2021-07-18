da4ed594725009467e56ffed8d44c7038417c4fe
jacob <4974971+jscotroby@users.noreply.github.com>
2021-07-18 14:49:53 -0700

    Modify naming conventions on sdk

--- lib/pkg/framework/src/SDK.ts
+++ lib/pkg/framework/src/SDK.ts
@@ -48,48 +48,45 @@ class SDK {
     }
   };
 
-  public integration = {
-    get: (integrationId: string) => this.v2Request(HTTPMethod.GET, `/integration/${integrationId}`),
-    delete: (integrationId: string) => this.v2Request(HTTPMethod.DELETE, `/integration/${integrationId}`),
-    put: (integrationId: string, body: any) => this.v2Request(HTTPMethod.PUT, `/integration/${integrationId}`, body),
-    post: (integrationId: string, body: any) => this.v2Request(HTTPMethod.POST, `/integration/`, body),
-    session: {
-      get: (integrationId: string, sessionId: string) =>
-        this.v2Request(HTTPMethod.GET, `/integration/${integrationId}/session/${sessionId}`),
-      delete: (integrationId: string, sessionId: string) =>
-        this.v2Request(HTTPMethod.DELETE, `/integration/${integrationId}/session/${sessionId}`),
-      put: (integrationId: string, sessionId: string, body: any) =>
-        this.v2Request(HTTPMethod.PUT, `/integration/${integrationId}/session/${sessionId}`, body),
-      post: (integrationId: string, sessionId: string, body: any) =>
-        this.v2Request(HTTPMethod.POST, `/integration/${integrationId}/session`, body),
-    },
-    dispatch: async (integrationId: string, method: HTTPMethod, path: string, body?: any) =>
-      this.v2Request(method, `/integration/${integrationId}${path}`, body),
-  };
-  public connector = {
-    get: (connectorId: string) => this.v2Request(HTTPMethod.GET, `/connector/${connectorId}`),
-    delete: (connectorId: string) => this.v2Request(HTTPMethod.DELETE, `/connector/${connectorId}`),
-    put: (connectorId: string, body: any) => this.v2Request(HTTPMethod.PUT, `/connector/${connectorId}`, body),
-    post: (connectorId: string, body: any) => this.v2Request(HTTPMethod.POST, `/connector/${connectorId}`, body),
-    session: {
-      get: (connectorId: string, sessionId: string) =>
-        this.v2Request(HTTPMethod.GET, `/connector/${connectorId}/session/${sessionId}`),
-      delete: (connectorId: string, sessionId: string) =>
-        this.v2Request(HTTPMethod.DELETE, `/connector/${connectorId}/session/${sessionId}`),
-      put: (connectorId: string, sessionId: string, body: any) =>
-        this.v2Request(HTTPMethod.PUT, `/connector/${connectorId}/session/${sessionId}`, body),
-      post: (connectorId: string, sessionId: string, body: any) =>
-        this.v2Request(HTTPMethod.POST, `/connector/${connectorId}/session`, body),
-    },
-    dispatch: async (connectorId: string, method: HTTPMethod, path: string, body?: any) =>
-      this.v2Request(method, `/connector/${connectorId}${path}`, body),
-  };
-  public operation = {
-    get: (operationId: string) => this.v2Request(HTTPMethod.GET, `/operation/${operationId}`),
-    delete: (operationId: string) => this.v2Request(HTTPMethod.DELETE, `/operation/${operationId}`),
-    put: (operationId: string, body: any) => this.v2Request(HTTPMethod.PUT, `/operation/${operationId}`, body),
-    post: (operationId: string, body: any) => this.v2Request(HTTPMethod.POST, `/operation/${operationId}`, body),
-  };
+  public getIntegration = (integrationId: string) => this.v2Request(HTTPMethod.GET, `/integration/${integrationId}`);
+  public deleteIntegration = (integrationId: string) =>
+    this.v2Request(HTTPMethod.DELETE, `/integration/${integrationId}`);
+  public updateIntegration = (integrationId: string, body: any) =>
+    this.v2Request(HTTPMethod.PUT, `/integration/${integrationId}`, body);
+  public createIntegration = (integrationId: string, body: any) =>
+    this.v2Request(HTTPMethod.POST, `/integration/`, body);
+  public createIntegrationSession = (integrationId: string, sessionId: string, body: any) =>
+    this.v2Request(HTTPMethod.POST, `/integration/${integrationId}/session`, body);
+  public updateIntegrationSession = (integrationId: string, sessionId: string, body: any) =>
+    this.v2Request(HTTPMethod.PUT, `/integration/${integrationId}/session/${sessionId}`, body);
+  public getIntegrationSession = (integrationId: string, sessionId: string) =>
+    this.v2Request(HTTPMethod.GET, `/integration/${integrationId}/session/${sessionId}`);
+  public deleteIntegrationSession = (integrationId: string, sessionId: string) =>
+    this.v2Request(HTTPMethod.DELETE, `/integration/${integrationId}/session/${sessionId}`);
+  public callIntegration = async (integrationId: string, method: HTTPMethod, path: string, body?: any) =>
+    this.v2Request(method, `/integration/${integrationId}${path}`, body);
+
+  public getConnector = (connectorId: string) => this.v2Request(HTTPMethod.GET, `/connector/${connectorId}`);
+  public deleteConnector = (connectorId: string) => this.v2Request(HTTPMethod.DELETE, `/connector/${connectorId}`);
+  public updateConnector = (connectorId: string, body: any) =>
+    this.v2Request(HTTPMethod.PUT, `/connector/${connectorId}`, body);
+  public createConnector = (connectorId: string, body: any) => this.v2Request(HTTPMethod.POST, `/connector/`, body);
+  public createConnectorSession = (connectorId: string, sessionId: string, body: any) =>
+    this.v2Request(HTTPMethod.POST, `/connector/${connectorId}/session`, body);
+  public updateConnectorSession = (connectorId: string, sessionId: string, body: any) =>
+    this.v2Request(HTTPMethod.PUT, `/connector/${connectorId}/session/${sessionId}`, body);
+  public getConnectorSession = (connectorId: string, sessionId: string) =>
+    this.v2Request(HTTPMethod.GET, `/connector/${connectorId}/session/${sessionId}`);
+  public deleteConnectorSession = (connectorId: string, sessionId: string) =>
+    this.v2Request(HTTPMethod.DELETE, `/connector/${connectorId}/session/${sessionId}`);
+  public callConnector = async (connectorId: string, method: HTTPMethod, path: string, body?: any) =>
+    this.v2Request(method, `/connector/${connectorId}${path}`, body);
+
+  public getOperation = (operationId: string) => this.v2Request(HTTPMethod.GET, `/operation/${operationId}`);
+  public deleteOperation = (operationId: string) => this.v2Request(HTTPMethod.DELETE, `/operation/${operationId}`);
+  public updateOperation = (operationId: string, body: any) =>
+    this.v2Request(HTTPMethod.PUT, `/operation/${operationId}`, body);
+  public createOperation = (operationId: string, body: any) => this.v2Request(HTTPMethod.POST, `/operation/`, body);
 }
 
 export default SDK;
