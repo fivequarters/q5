433fe0f3435f26d5aece89515bbf36f90c1f7d4d
degrammer <ruben@fusebit.io>
2021-07-19 15:27:19 -0500

    Add slack integration

---
+++ demo/slack-integration/integration.js
@@ -0,0 +1,34 @@
+const { Router } = require("@fusebit-int/framework");
+
+const router = new Router();
+
+router.get("/api/", async (ctx) => {
+  ctx.body = "Slack integration example works!";
+});
+
+router.post("/api/message/:identityId", async (ctx) => {
+  const identityId = ctx.params.identityId;
+  const message = ctx.req.body && ctx.req.body.message;
+
+  if (!message) {
+    ctx.throw(400, "Expected message");
+  }
+
+  const slackConnector = await ctx.state.manager.connectors.getByName(
+    "slack1",
+    (ctx) => identityId
+  )(ctx);
+
+  if (!slackConnector) {
+    ctx.throw(400, "Expected slack connector");
+  }
+
+  await slackConnector.slackClient.chat.postMessage({
+    text: message,
+    channel: "example-slack-connector-v2",
+  });
+
+  ctx.status = 201;
+});
+
+module.exports = router;
--- lib/pkg/slack-connector/src/index.ts
+++ lib/pkg/slack-connector/src/index.ts
@@ -1,7 +1,22 @@
-import { Context, Next, Router } from '@fusebit-int/framework';
+/**
+ * Fusebit, Inc. Slack Connector
+ */
+import {IOnStartup, Next, Router } from '@fusebit-int/framework'; // TODO: Export this from the oauth connector
+const OAuthConnectorRouter = require('@fusebit-int/pkg-oauth-connector');
+
+import superagent from 'superagent';
+const { WebClient } = require('@slack/web-api');
 
 const router = new Router();
+// 1. Inject common configuration values for consuming slack sdk.
 
-router.get('/api', (ctx: Context) => {
-  ctx.body = 'hello world';
+router.on('startup', async ({ mgr, cfg, router: rtr }: IOnStartup, next: Next) => { 
+  cfg.configuration.channel = 'example-slack-connector-v2';
+  cfg.configuration.scope = 'chat:write';
+  cfg.configuration.tokenUrl = 'https://slack.com/api/oauth.v2.access';
+  cfg.configuration.authorizationUrl = 'https://slack.com/oauth/v2/authorize';
+  return next();
 });
+
+router.use(OAuthConnectorRouter.routes());
+module.exports = router;
