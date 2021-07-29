129b9cda2fb8ce27cc5e127313dc46f21b57bb47
degrammer <ruben@fusebit.io>
2021-07-29 13:54:11 -0500

    Return single entity for connector instance

--- lib/pkg/slack-integration/src/SlackIntegration.ts
+++ lib/pkg/slack-integration/src/SlackIntegration.ts
@@ -1,13 +1,13 @@
 import { Context, IntegrationActivator } from '@fusebit-int/framework';
 import { WebClient } from '@slack/web-api';
 
-export default class SlackIntegration extends IntegrationActivator<{ slackClient: WebClient }> {
+export default class SlackIntegration extends IntegrationActivator<WebClient> {
   /*
    * This function will create an authorized wrapper of the Slack SDK client.
    */
-  protected async instantiate(ctx: Context, lookupKey: string): Promise<{ slackClient: WebClient }> {
+  protected async instantiate(ctx: Context, lookupKey: string): Promise<WebClient> {
     const token = await this.requestConnectorToken({ ctx, lookupKey });
     const slackClient = new WebClient(token);
-    return { slackClient };
+    return slackClient;
   }
 }
