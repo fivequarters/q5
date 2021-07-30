a9e615b17f74b1a7a0f6283a133a705e45cb053e
jacob <4974971+jscotroby@users.noreply.github.com>
2021-07-30 11:46:44 -0700

    wip

--- lib/pkg/slack-connector/src/index.ts
+++ lib/pkg/slack-connector/src/index.ts
@@ -1,13 +1,39 @@
 /**
  * Fusebit, Inc. Slack Connector
  */
-import { Context, IOnStartup, Middleware, Next, Router, Form } from '@fusebit-int/framework';
+import { Context, IOnStartup, Middleware, Next, Router, Manager } from '@fusebit-int/framework';
 const OAuthConnectorRouter = require('@fusebit-int/pkg-oauth-connector');
 import { schema, uischema } from './form';
 
 const router = new Router();
 const TOKEN_URL = 'https://slack.com/api/oauth.v2.access';
 const AUTHORIZATION_URL = 'https://slack.com/oauth/v2/authorize';
+
+let a: Router;
+let b: Router;
+const routeCombiner = (() => {
+  let resolvers: (() => void)[] = [];
+  const promiseA: Promise<void> = new Promise((resolver) => resolvers.push(resolver));
+  const promiseB: Promise<void> = new Promise((resolver) => resolvers.push(resolver));
+  return {
+    promise: (async () => {
+      await Promise.all([promiseA, promiseB]);
+      a!.use(b!.routes());
+      return a!;
+    })(),
+    resolvers,
+  };
+})();
+
+const setA = (router: Router) => {
+  a = router;
+  routeCombiner.resolvers[0]();
+};
+
+const setB = (router: Router) => {
+  b = router;
+  routeCombiner.resolvers[0]();
+};
 
 interface ISettings {
   scope: string;
@@ -65,6 +91,7 @@ const mapConfiguration = (configuration: any): IConfigurationSettings => {
 router.on('startup', async ({ mgr, cfg, router: rtr }: IOnStartup, next: Next) => {
   cfg.configuration.tokenUrl = TOKEN_URL;
   cfg.configuration.authorizationUrl = AUTHORIZATION_URL;
+  setA(rtr);
   return next();
 });
 
@@ -77,4 +104,5 @@ router.get('/api/configure', Middleware.authorize('connector:put'), async (ctx: 
 });
 
 router.use(OAuthConnectorRouter.routes());
+setB(router);
 module.exports = router;
