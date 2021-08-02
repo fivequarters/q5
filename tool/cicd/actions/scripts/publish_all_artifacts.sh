5a8c86e923469e4f97f82e8f5e42025d8842cba1
Matthew Zhao <matthew@fusebit.io>
2021-08-02 15:25:23 -0700

    publish all artifacts

---
+++ tool/cicd/actions/scripts/publish_all_artifacts.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+cd lib/pkg
+
+FUSE="node ../../cli/fusebit-cli/libc/index.js"
+${FUSE} profile set cicd
+${FUSE} npm login
+for pkgPath in framework ./pkg-* ./slack-*; do
+  pkgName=$(basename $pkgPath)
+  cd ${pkgName};
+  yarn build;
+  npm publish;
+  cd ..
+done
+
+${FUSE} profile set cicd
+
+cd ../..
