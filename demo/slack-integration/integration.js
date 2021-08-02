6f7bd9a5da0ec394a32dabc4ce8c50c54b3333af
jacob <4974971+jscotroby@users.noreply.github.com>
2021-08-02 13:35:12 -0700

    demo updates
    peel out secrets

--- demo/slack-integration/integration.js
+++ demo/slack-integration/integration.js
@@ -3,32 +3,37 @@ const Integration = require("@fusebit-int/integration");
 const integration = new Integration();
 const router = integration.router;
 
-router.get("/api", async (ctx) => {
-  ctx.body = "Slack integration example works!";
-});
+integration.router.post(
+  "/api/:tenantId/message",
+  integration.middleware.authorizeUser(integration.Permissions.storageAll),
+  async (ctx) => {
+    const message = ctx.req.body?.message;
+    if (!message) {
+      ctx.throw(400, "Expected message");
+    }
 
-router.post("/api/message/:identityId", async (ctx) => {
-  const identityId = ctx.params.identityId;
-  const message = ctx.req.body?.message;
+    const WebClient = await integration.service.getSDK(
+      ctx,
+      "slack1",
+      "tenantId"
+    );
+    if (!WebClient) {
+      ctx.throw(400, "Expected slack connector");
+    }
 
-  if (!message) {
-    ctx.throw(400, "Expected message");
+    const instances = integration.service.listTenantInstances(ctx);
+
+    await Promise.all(
+      instances.map(async (instance) => {
+        await WebClient.chat.postMessage({
+          text: message,
+          channel: "example-slack-connector-v2",
+          userId: (instance) => string,
+        });
+      })
+    );
+
+    ctx.body = { message: "success" };
   }
-
-  const slackConnector = await integration.service.getByName(
-    "slack1",
-    (ctx) => identityId
-  )(ctx);
-
-  if (!slackConnector) {
-    ctx.throw(400, "Expected slack connector");
-  }
-
-  await slackConnector.slackClient.chat.postMessage({
-    text: message,
-    channel: "example-slack-connector-v2",
-  });
-
-  ctx.status = 201;
-});
+);
 module.exports = integration;
