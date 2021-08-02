4946b9f485646a6c63ed279175430ec2ae0a08ef
Matthew Zhao <matthew@fusebit.io>
2021-08-01 21:22:24 -0700

    cicd

--- tool/cicd/actions/scripts/test_function_api.sh
+++ tool/cicd/actions/scripts/test_function_api.sh
@@ -4,5 +4,4 @@
 set -e
 
 cd api/function-api
-export FUSE_PROFILE=`echo ${SECRET_FUSEBIT_PROFILE} | jq -r '.user.profileName'`
-EC2=1 yarn test --testPathIgnorePatterns test/cron test/npm
+EC2=1 yarn test
