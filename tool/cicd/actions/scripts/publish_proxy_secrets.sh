efe4829c8b8d97d2105cb8b3bca526c8a7cc1874
Matthew Zhao <matthew@fusebit.io>
2021-08-09 12:02:36 -0700

    oops

--- tool/cicd/actions/scripts/publish_proxy_secrets.sh
+++ tool/cicd/actions/scripts/publish_proxy_secrets.sh
@@ -8,7 +8,7 @@ VERSION_FUSEBIT_OPS_CLI=$(cat cli/fusebit-ops-cli/package.json | jq -r .version)
 # Install tools
 npm install -g https://cdn.fusebit.io/fusebit/cli/fusebit-ops-cli-v${VERSION_FUSEBIT_OPS_CLI}.tgz
 
-PAYLOAD="{\"proxy\":{\"slack\":{\"clientId\"\:\"${SLACK_PROXY_CLIENT_ID}\",\"clientSecret\"\:\"${SLACK_PROXY_CLIENT_SECRET}\"}}}"
+PAYLOAD="{\"proxy\":{\"slack\":{\"clientId\":\"${SLACK_PROXY_CLIENT_ID}\",\"clientSecret\":\"${SLACK_PROXY_CLIENT_SECRET}\"}}}"
 
 # 749
 fuse-ops profile default github-automation.749
