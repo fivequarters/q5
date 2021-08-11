abf2174e8539021cc8a7b29a1d90539eefde4627
Benn Bollay <bb.github@magitech.org>
2021-08-11 13:54:23 -0700

    Update the catalog to connect all the dots (#1078)

--- site/catalog/src/markdown.ts
+++ site/catalog/src/markdown.ts
@@ -3,7 +3,7 @@ import { join } from 'path';
 
 const importDir = 'markdown';
 
-const loadImports = async () => {
+export const loadImports = async () => {
   const files = await readDirectory(importDir, { filesOnly: true });
 
   const result: Record<string, string> = {};
@@ -15,22 +15,33 @@ const loadImports = async () => {
   return result;
 };
 
-export const generateMarkdown = async (input: string) => {
+export const generateMarkdown = (
+  sourceFileName: string,
+  input: string,
+  includeMeta: boolean,
+  imports: Record<string, string>
+) => {
   const lines = input.split('\n');
 
   const output: string[] = [];
-
-  const imports = await loadImports();
 
   const cmdRe = new RegExp('^\\[//]: # \\(([A-Z]*): ([A-Z]*)\\)$');
 
   lines.forEach((line) => {
     const match = line.match(cmdRe);
 
-    if (match && match[1] === 'IMPORT' && imports[match[2]]) {
-      output.push('', `[//]: # (START: ${match[2]})`, '');
+    if (match && match[1] === 'IMPORT') {
+      if (!imports[match[2]]) {
+        console.error(`[WARN] ${sourceFileName}: Unknown import ${match[2]}; dropping line.`);
+        return;
+      }
+      if (includeMeta) {
+        output.push('', `[//]: # (START: ${match[2]})`, '');
+      }
       output.push(imports[match[2]]);
-      output.push('', `[//]: # (END: ${match[2]})`, '');
+      if (includeMeta) {
+        output.push('', `[//]: # (END: ${match[2]})`, '');
+      }
     } else {
       output.push(line);
     }
