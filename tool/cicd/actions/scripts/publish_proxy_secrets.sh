6c71e061c20151a74967cfa03430e2a3bcb77487
Matthew Zhao <matthew@fusebit.io>
2021-08-11 11:55:54 -0700

    xd

--- tool/cicd/actions/scripts/publish_proxy_secrets.sh
+++ tool/cicd/actions/scripts/publish_proxy_secrets.sh
@@ -10,14 +10,14 @@ npm install -g https://cdn.fusebit.io/fusebit/cli/fusebit-ops-cli-v${VERSION_FUS
 
 # 749
 fuse-ops profile default github-automation.749
-fuse-ops deployment defaults set dev subscriptions '{\"proxy\":{\"accountId":\"acc-5e58f2ccd79444e4\",\"subscriptionId\":\"sub-9239585cf84c450a\"}}'
+fuse-ops deployment defaults set dev subscription '{\"proxy\":{\"accountId":\"acc-5e58f2ccd79444e4\",\"subscriptionId\":\"sub-9239585cf84c450a\"}}'
 
 
 # 321
 fuse-ops profile default github-automation.321
-fuse-ops deployment defaults set stage subscriptions '{\"proxy\":{\"accountId\":\"acc-2ec6c8dba6134772\",\"subscriptionId\":\"sub-2e2374b63eb040e9\"}}'
-fuse-ops deployment defaults set api subscriptions '{\"proxy\":{\"accountId\":\"acc-828c6c45cba94d93\",\"subscriptionId\":\"sub-b25bdcca338b4c32\"}}' --us-east-1
-fuse-ops deployment defaults set api subscriptions '{\"proxy\":{\"accountId\":\"acc-49c6c6c2f60c4867\",\"subscriptionId\":\"sub-b7efb0965d2a42dc\"}}' --region us-west-1
+fuse-ops deployment defaults set stage subscription '{\"proxy\":{\"accountId\":\"acc-2ec6c8dba6134772\",\"subscriptionId\":\"sub-2e2374b63eb040e9\"}}'
+fuse-ops deployment defaults set api subscription '{\"proxy\":{\"accountId\":\"acc-828c6c45cba94d93\",\"subscriptionId\":\"sub-b25bdcca338b4c32\"}}' --us-east-1
+fuse-ops deployment defaults set api subscription '{\"proxy\":{\"accountId\":\"acc-49c6c6c2f60c4867\",\"subscriptionId\":\"sub-b7efb0965d2a42dc\"}}' --region us-west-1
 
 # 321 stage
 curl -H "Authorization: Bearer `fuse token -o raw`" https://stage.us-west-2.fusebit.io/v1/account/acc-2ec6c8dba6134772/subscription/sub-2e2374b63eb040e9/storage/proxy/slack/configuration -XPUT -d "{\"clientId\":\"${SLACK_PROXY_CLIENT_ID}\",\"clientSecret\":\"${SLACK_PROXY_CLIENT_SECRET}\"}"
