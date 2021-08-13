b78ae855ef1697f20fbb476152f3f9f308e99e4f
Matthew Zhao <46915203+matthewzhaocc@users.noreply.github.com>
2021-08-12 17:00:33 -0700

    ENG-288 Add Scripts for Global Secrets (#1068)

---
+++ tool/cicd/actions/scripts/publish_proxy_secrets.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -xe
+
+# Get package version
+VERSION_FUSEBIT_OPS_CLI=$(cat cli/fusebit-ops-cli/package.json | jq -r .version)
+VERSION_FUSEBIT_CLI=$(cat cli/fusebit-cli/package.json | jq -r .version)
+
+# Install tools
+npm install -g https://cdn.fusebit.io/fusebit/cli/fusebit-ops-cli-v${VERSION_FUSEBIT_OPS_CLI}.tgz
+npm install -g @fusebit/cli@${VERSION_FUSEBIT_CLI}
+
+# 749
+fuse-ops profile default github-automation.749
+fuse-ops deployment defaults set dev subscription \{\"proxy\":\{\"accountId\":\"acc-5e58f2ccd79444e4\",\"subscriptionId\":\"sub-9239585cf84c450a\"\}\}
+
+
+# 321
+fuse-ops profile default github-automation.321
+fuse-ops deployment defaults set stage subscription \{\"proxy\":\{\"accountId\":\"acc-2ec6c8dba6134772\",\"subscriptionId\":\"sub-2e2374b63eb040e9\"\}\} --region us-west-2
+fuse-ops deployment defaults set api subscription \{\"proxy\":\{\"accountId\":\"acc-828c6c45cba94d93\",\"subscriptionId\":\"sub-b25bdcca338b4c32\"\}\} --region us-east-1
+fuse-ops deployment defaults set api subscription \{\"proxy\":\{\"accountId\":\"acc-49c6c6c2f60c4867\",\"subscriptionId\":\"sub-b7efb0965d2a42dc\"\}\} --region us-west-1
+
+SECRET_PAYLOAD="{\"data\":{\"clientId\":\"${SLACK_PROXY_CLIENT_ID}\",\"clientSecret\":\"${SLACK_PROXY_CLIENT_SECRET}\"}}"
+
+# 321 stage
+curl -H "Authorization: Bearer `fuse token -o raw`" https://stage.us-west-2.fusebit.io/v1/account/acc-2ec6c8dba6134772/subscription/sub-2e2374b63eb040e9/storage/proxy/slack/configuration -XPUT -d ${SECRET_PAYLOAD} -H "Content-Type: application/json"
