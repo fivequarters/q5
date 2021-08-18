7b8334eb497f6f141d78b5c6d2052531ac469197
Benn Bollay <bb.github@magitech.org>
2021-08-17 22:56:33 -0700

    Remove file

--- site/catalog/feed_integration/slack-bot/integration/integration.js
+++ site/catalog/feed_integration/slack-bot/integration/integration.js
@@ -1,42 +1,61 @@
-// Fusebit Slack Integration
-//
-// Create a simple bot to communicate about events in your product with your customers, over their very own
-// Slack.  This template shows you how easy it is to create your very own Fusebit Integration and use the
-// official Slack SDK - without any of the work!
-//
-// After reading through this integration, you will be able to create an integration, enable your customers to
-// approve it for use in their Slack workspaces, and send messages to your customers based on events that
-// happen in your infrastructure or elsewhere on your product.
-//
-// Learn more about Fusebit Integrations at: https://developer.fusebit.io/docs/integration-programming-model
+/*
+ * Slack Integration
+ * Integrate, Deploy and maintain a Slack SDK into your application.
+ * Learn more about integrations at https://developer.fusebit.io/docs/integration-programming-model
+ */
 
 const { Integration } = require('@fusebit-int/framework');
 
+/* In order to have access to our SDK methods you need to create a new instance of Integration class.  */
 const integration = new Integration();
 
-// Fusebit leverages the very popular Router concept, as used by both Express and KoaJS.
+/*
+ * Since you're building an API, you need to expose routes, so your application can determine how to respond
+ * to client requests to a particular endpoint via HTTP request methods (GET, POST, PUT, DELETE)
+ */
 const router = integration.router;
 
-// Allow only authorized clients (such as your backend) to send a test message to a tenant.
-router.post('/api/tenant/:tenantId/test', integration.middleware.authorizeUser('instance:get'), async (ctx) => {
-  // Create an official Slack SDK instance, already authorized with the tenant's credentials
+/* Get some inspiration from the following example endpoints and learn how to use the official Slack SDK. */
+
+/*
+ * Post a message to Slack
+ * In this endpoint you will find the following components:
+ * 1. Defining a route path: /api/tenant/:tenantId/test
+ *    This is a test endpoint using the POST HTTP method, notice the :tenantId parameter
+ *    which represents an user from your integration, this can be one of your customers for example.
+ * 2. Protecting your route
+ *    The second parameter of the route is the place where you can specify the middleware, you can write your
+ *    own or use our provided middleware like the one we're using here for checking authorization of performing this action.
+ */
+router.post('/api/tenant/:tenantId/test', integration.middleware.authorizeUser('instance:put'), async (ctx) => {
+  /*
+   * You can use the Slack SDK by referencing the connector name 'slackConnector' you provided in the data.component[0].name field.
+   * By calling getSdkByTenant you will get a configured Slack web client with all the available Slack Web API methods.
+   * Now you just focus on your core application and we will handle the rest from our SDK!
+   * All of the Slack Web API is available here, you can read about them at https://www.npmjs.com/package/@slack/web-api
+   */
   const slackClient = await integration.tenant.getSdkByTenant(ctx, 'slackConnector', ctx.params.tenantId);
 
-  // Send a message! Try replacing the text and the channel name with something different :)
+  /* Use the Slack Web API to send a message to a specific channel name */
   const result = await slackClient.chat.postMessage({
     text: 'Hello world from Fusebit!',
-    channel: '#general',
+    channel: '<replace-with-channel-name-here>',
   });
 
+  /*
+   * Write the result to the body and send it to the client request.
+   * This is how you send content back to the browser from this route.
+   */
   ctx.body = result;
 });
 
-// Instead of sending a message, list all of the active users and return that information to the caller.
+/*
+ * List Slack users
+ * Following a similar approach from the previous method, you can use the Slack SDK for read operations too.
+ */
 router.get('/api/tenant/:tenantId/users', integration.middleware.authorizeUser('instance:get'), async (ctx) => {
   const slackClient = await integration.tenant.getSdkByTenant(ctx, 'slackConnector', ctx.params.tenantId);
-
   const result = await slackClient.users.list();
-
   ctx.body = result;
 });
 
