d5c6e65761ba63dfed7b2b0e9c6f7e04c2aaf413
Matthew Zhao <matthew@fusebit.io>
2021-09-01 13:18:48 -0700

    .

--- tool/cicd/actions/scripts/publish_all_pkg_versioned.sh
+++ tool/cicd/actions/scripts/publish_all_pkg_versioned.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-cd lib/pkg
+HOMEDIR=`pwd`
 
-FUSE="node ../../cli/fusebit-cli/libc/index.js"
+cd lib/pkg
+pwd
+FUSE="node ${HOMEDIR}/cli/fusebit-cli/libc/index.js"
 
 PACKAGE_FILES=`find . -name node_modules -prune -false -o -name package.json`;
 BASEDIR=`pwd`
-for fuseProfile in selfservice.api.us-west-1.internal stage.us-west-2.internal; do
+for fuseProfile in stage.fusebit.io; do
   ${FUSE} profile set ${fuseProfile}
   ${FUSE} npm login
   for pkgPath in ${PACKAGE_FILES}; do
     dirName=$(dirname $pkgPath)
     cd ${dirName};
+
     echo BUILDING ${dirName}
     VERSION=`jq -rc ".version" package.json`
     dirName=$(dirname $pkgPath)
     pkgName="$(basename -- $dirName)"
-
     git tag --points-at HEAD | grep ${pkgName}-${VERSION} > /dev/null
     TAG_TEST=$?
     if [ ${TAG_TEST} -ne 0 ]; then
-      echoerr "Not publishing ${VERSION} - HEAD is not tagged ${pkgName}-${VERSION}"
-      git tag --points-at HEAD
+      echo "Not publishing ${VERSION} - HEAD is not tagged ${pkgName}-${VERSION}"
       continue
     else
-        echoerr "Publishing ${VERSION}"
+        echo "Publishing ${VERSION}"
     fi
     yarn build;
     npm publish;
-    cd ${BASEDIR}
+    cd ${pkgDir}
   done
 done
 cd ../..
