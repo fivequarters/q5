9c2815ff37b38b092f007815060e6d58baffdcc8
Matthew Zhao <matthew@fusebit.io>
2021-09-07 23:25:14 -0700

    .

---
+++ tool/cicd/slack-notification/src/SlackNotification.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
+
+export default (filePath: string) => {};
--- tool/cicd/slack-notification/src/index.ts
+++ tool/cicd/slack-notification/src/index.ts
@@ -1,1 +1,18 @@
-export { default as NewClass } from './NewClass';
+#!/usr/bin/env node
+import fs from 'fs';
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
+
+const processInputAndTriggerNotification = async () => {
+  const argv = await yargs(hideBin(process.argv)).argv;
+
+  if (!argv || !argv.path || typeof argv.path !== 'string') {
+    throw Error('Invalid input path.');
+  }
+  const content = await fs.promises.readFile(argv.path);
+  return JSON.parse(content.toString('utf8'));
+};
+
+processInputAndTriggerNotification().then((pl) => {
+  console.log(pl);
+});
