03dcca8ab0d2e590acad8a9b0c5bbbcec14d1b62
Bruno Krebs <krebs.bruno@gmail.com>
2021-09-07 22:11:30 -0300

    more cleaning up;

--- api/function-api/src/routes/validation/integration.ts
+++ api/function-api/src/routes/validation/integration.ts
@@ -33,13 +33,6 @@ const Data = Joi.alternatives().try(
       )
       .unique((a: { name: string }, b: { name: string }) => a.name === b.name)
       .default([]),
-    schedule: Joi.array().items(
-      Joi.object({
-        cron: Joi.string().required(),
-        timezone: Joi.string(),
-        endpoint: Joi.string().required(),
-      })
-    ),
   }),
   Joi.object().keys({})
 );
