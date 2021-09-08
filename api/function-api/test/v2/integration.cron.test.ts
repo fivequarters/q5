a00f5bf664e1a9186cd54274a1c6d97a07ea9704
Bruno Krebs <krebs.bruno@gmail.com>
2021-09-08 14:35:47 -0300

    reorganizing tests;

--- api/function-api/test/v2/integration.cron.test.ts
+++ api/function-api/test/v2/integration.cron.test.ts
@@ -1,5 +1,6 @@
-import { cleanupEntities, ApiRequestMap, v2Request, IRequestOptions, RequestMethod } from './sdk';
+import { cleanupEntities, ApiRequestMap } from './sdk';
 import { getEnv } from '../v1/setup';
+import { getFunction } from '../v1/sdk';
 
 let { account, boundaryId } = getEnv();
 beforeEach(() => {
@@ -10,65 +11,71 @@ afterAll(async () => {
   await cleanupEntities(account);
 }, 180000);
 
-const getScheduledIntegration = () => {
-  return {
-    data: {
-      handler: './integrationTest.js',
-      schedule: [
-        {
-          cron: '* * * * *',
-          endpoint: '/api/scheduled',
-        },
-      ],
-      files: {
-        ['package.json']: JSON.stringify({
-          scripts: {},
-          dependencies: {
-            ['@fusebit-int/framework']: '4.0.1',
-          },
-          files: ['./integrationTest.js'],
-        }),
-        ['integrationTest.js']: [
-          "const { Integration } = require('@fusebit-int/framework');",
-          '',
-          'const integration = new Integration();',
-          'const router = integration.router;',
-          '',
-          "router.cron('/api/scheduled', async (ctx) => {",
-          '  ctx.body = process.version;',
-          '});',
-          '',
-          'module.exports = integration;',
-        ].join('\n'),
-      },
-    },
-    id: boundaryId,
-  };
-};
-
 describe('Scheduled integrations', () => {
-  test('Creating a scheduled integration', async () => {
-    // create a scheduled integration
-    const scheduledIntegration = getScheduledIntegration();
-    const createIntegrationResponse = await ApiRequestMap.integration.postAndWait(
-      account,
-      scheduledIntegration.id,
-      scheduledIntegration
-    );
-    expect(createIntegrationResponse).toBeHttp({ statusCode: 200 });
+  test('Creating an every-minute scheduled integration', async () => {
+    const cron = '* * * * *';
+    await putScheduledIntegration(boundaryId, cron);
+    await validateIntegrationSpec(boundaryId, cron);
+  }, 180000);
 
-    // check its definition
-    const requestOptions: IRequestOptions = {
-      uri: `/integration/${scheduledIntegration.id}`,
-      method: RequestMethod.get,
+  test('Creating an hourly scheduled integration', async () => {
+    const cron = '0 * * * *';
+    await putScheduledIntegration(boundaryId, cron);
+    await validateIntegrationSpec(boundaryId, cron);
+  }, 180000);
+
+  test('Creating a daily integration', async () => {
+    const cron = '15 7 * * *';
+    await putScheduledIntegration(boundaryId, cron);
+    await validateIntegrationSpec(boundaryId, cron);
+  }, 180000);
+
+  const putScheduledIntegration = async (id: string, cron: string) => {
+    const integrationSpec = {
+      data: {
+        handler: './integration.js',
+        schedule: [{ cron, endpoint: '/api/scheduled' }],
+        files: {
+          ['package.json']: JSON.stringify({
+            dependencies: {
+              ['@fusebit-int/framework']: '4.0.1',
+            },
+            files: ['./integration.js'],
+          }),
+          ['integration.js']: [
+            "const { Integration } = require('@fusebit-int/framework');",
+            '',
+            'const integration = new Integration();',
+            'const router = integration.router;',
+            '',
+            "router.cron('/api/scheduled', async (ctx) => {",
+            '  ctx.body = "Hi there";',
+            '});',
+            '',
+            'module.exports = integration;',
+          ].join('\n'),
+        },
+      },
+      id,
     };
-    const { data: responseBody } = await v2Request(account, requestOptions);
-    expect(responseBody).toBeDefined();
+    const createIntegrationResponse = await ApiRequestMap.integration.postAndWait(account, id, integrationSpec);
+    expect(createIntegrationResponse).toBeHttp({ statusCode: 200 });
+  };
 
-    const { data: integrationSpec } = responseBody;
+  const validateIntegrationSpec = async (id: string, cronConfig: string) => {
+    // check v1 underneath function config
+    const integrationBoundary = 'integration';
+    const { data: functionSpec } = await getFunction(account, integrationBoundary, id);
+    expect(functionSpec).toBeDefined();
+    expect(functionSpec.schedule).toBeDefined();
+    expect(functionSpec.schedule.cron).toBe(cronConfig);
 
+    // check v2 config
+    const integrationResponse = await ApiRequestMap.integration.get(account, id);
+    expect(integrationResponse).toBeHttp({ statusCode: 200 });
+    const integrationSpec = integrationResponse.data.data;
     expect(integrationSpec).toBeDefined();
     expect(integrationSpec.schedule).toBeDefined();
-    expect(integrationSpec.schedule.cron).toBe(scheduledIntegration.data.schedule[0].cron);
-  }, 180000);
+    expect(integrationSpec.schedule.cron).toBe(cronConfig);
+  };
 });
