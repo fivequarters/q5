a63ba0c23e064fdadf3eb6336fa84c6c091fa5dd
Matthew Zhao <matthew@fusebit.io>
2021-09-16 15:53:56 -0700

    :wq

--- tool/cicd/profile/createProfileBundle.js
+++ tool/cicd/profile/createProfileBundle.js
@@ -1,26 +1,43 @@
 #!/usr/bin/env -S node --no-warnings
 
-const assert = require('assert');
-const fs = require('fs');
-const path = require('path');
-const homedir = require('os').homedir();
+const assert = require("assert");
+const fs = require("fs");
+const path = require("path");
+const homedir = require("os").homedir();
 
 const userProfileRoot = `${homedir}/.fusebit`;
-const userProfileNames = ['stage.us-west-2.internal', 'selfservice.api.us-west-1.internal'];
+const userProfileNames = [
+  "stage.us-west-2.internal",
+  "selfservice.api.us-west-1.internal",
+];
 const opsProfileRoot = `${homedir}/.fusebit-ops`;
-const opsProfileNames = ['github-automation.321', 'github-automation.749', 'github-automation.763'];
+const opsProfileNames = [
+  "github-automation.321",
+  "github-automation.749",
+  "github-automation.763",
+];
 
 const renderUserProfile = () => {
-  const settings = JSON.parse(fs.readFileSync(path.join(userProfileRoot, 'settings.json')));
+  const settings = JSON.parse(
+    fs.readFileSync(path.join(userProfileRoot, "settings.json"))
+  );
   const profiles = {};
   const keys = {};
   for (const userProfileName of userProfileNames) {
     const kid = settings.profiles[userProfileName].kid;
-    const privateKey = fs.readFileSync(path.join(userProfileRoot, 'keys', userProfileName, kid, 'pri')).toString();
-    const publicKey = fs.readFileSync(path.join(userProfileRoot, 'keys', userProfileName, kid, 'pub')).toString();
+    const privateKey = fs
+      .readFileSync(
+        path.join(userProfileRoot, "keys", userProfileName, kid, "pri")
+      )
+      .toString();
+    const publicKey = fs
+      .readFileSync(
+        path.join(userProfileRoot, "keys", userProfileName, kid, "pub")
+      )
+      .toString();
     profiles[userProfileName] = settings.profiles[userProfileName];
-    keys[path.join('keys', userProfileName, kid, 'pri')] = privateKey;
-    keys[path.join('keys', userProfileName, kid, 'pub')] = publicKey;
+    keys[path.join("keys", userProfileName, kid, "pri")] = privateKey;
+    keys[path.join("keys", userProfileName, kid, "pub")] = publicKey;
   }
 
   const output = {
@@ -36,7 +53,9 @@ const renderUserProfile = () => {
 };
 
 const renderOpsProfile = () => {
-  const settings = JSON.parse(fs.readFileSync(path.join(opsProfileRoot, 'settings.json')));
+  const settings = JSON.parse(
+    fs.readFileSync(path.join(opsProfileRoot, "settings.json"))
+  );
   const output = {
     settings: {
       profiles: {},
