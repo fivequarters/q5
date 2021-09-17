7c666f7f939b83c7b03f221df2d832bcb990e7d0
Matthew Zhao <46915203+matthewzhaocc@users.noreply.github.com>
2021-09-17 07:10:03 -0700

    Update clean_env.sh

--- tool/cicd/jenkins/scripts/clean_env.sh
+++ tool/cicd/jenkins/scripts/clean_env.sh
@@ -40,7 +40,7 @@ done
 
 # AWS envs
 REGION=us-east-2
-SECRET_ARN=arn:aws:secretsmanager:us-east-2:749775346857:secret:rds-db-credentials/fusebit-db-secret-cicd-ab3ddc2cbd4ff95e73df-tI8URB
-DATABASE_ARN=arn:aws:rds:us-east-2:749775346857:cluster:fusebit-db-cicd
+SECRET_ARN=arn:aws:secretsmanager:us-east-2:749775346857:secret:rds-db-credentials/fusebit-db-secret-jenkins-33e78e97c231edc47568
+DATABASE_ARN=arn:aws:rds:us-east-2:749775346857:cluster:fusebit-db-jenkins
 
 aws --region $REGION rds-data execute-statement --database fusebit --secret-arn $SECRET_ARN --resource-arn $DATABASE_ARN --sql "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
