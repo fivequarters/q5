5a71db6f9fb6a4b5e99126d57eb069e36ca15a6d
Matthew Zhao <matthew@fusebit.io>
2021-09-28 23:52:00 -0700

    Slack last 3 ppl

--- tool/cicd/slack-notification/src/index.ts
+++ tool/cicd/slack-notification/src/index.ts
@@ -2,11 +2,24 @@
 
 import superagent from 'superagent';
 import { JestOutput, Status } from './jestTypes';
+import fs from 'fs/promises';
 const outputJson: JestOutput = require('../../../../api/function-api/testOutput.json');
 
 // Using 2 variables despite being the same URL because eventually we want to publish to engineering on a test failure.
 const failureWebhook = 'https://hooks.slack.com/services/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';
 const successWebhook = 'https://hooks.slack.com/services/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';
+
+const nameToMention = [
+  { name: 'Matthew Zhao', id: '<@U01UDTF3VQR>' },
+  { name: 'Benn Bollay', id: '<@UUPT2SQN7>' },
+  { name: 'Ruben Restrepo', id: '<@U0277EMBRSN>' },
+  { name: 'Bruno Krebs', id: '<@U027X5JG8QG>' },
+  { name: 'Tomasz Janczuk', id: '<@UFN96HN1J>' },
+  { name: 'Yavor Georgiev', id: '<@UDGRLGJTG>' },
+  { name: 'Chris More', id: '<@U01NQDVLYKB>' },
+  { name: 'Shehzad Akbar', id: '<@U02CP37DEU8>' },
+  { name: 'Jacob Haller-Roby', id: '<@U01NQDVRW0Z>' },
+];
 
 (async () => {
   if (outputJson.success) {
@@ -47,6 +60,23 @@ const successWebhook = 'https://hooks.slack.com/services/TDFBLCJV9/B02ETT25989/f
       text: `Check the logs here ${process.env.BUILD_URL}`,
     },
   });
+
+  const commit = await fs.readFile('/var/lib/jenkins/workspace/fusebit-test-suite/commit.txt', 'utf8');
+  const commiters = commit.split('\n');
+  for (const commiter of commiters) {
+    for (const person of nameToMention) {
+      if (commiter.includes(person.name)) {
+        failurePayload.blocks.push({
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `${person.id} is one of the last commiters.`,
+          },
+        });
+      }
+    }
+  }
+
   try {
     await superagent.post(failureWebhook).send(failurePayload);
   } catch (e) {
