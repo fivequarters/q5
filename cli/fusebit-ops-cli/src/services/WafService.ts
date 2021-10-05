fb76856b7d7412d407dd098c06e8cbf6f612fa9b
Matthew Zhao <matthew@fusebit.io>
2021-10-05 14:40:18 -0700

    .

--- cli/fusebit-ops-cli/src/services/WafService.ts
+++ cli/fusebit-ops-cli/src/services/WafService.ts
@@ -4,7 +4,9 @@ import { AwsCreds, IAwsCredentials } from 'lib/aws/aws-cred/src/AwsCreds';
 import { IExecuteInput } from 'lib/node/cli/src/Command';
 import { ExecuteService } from '.';
 import { OpsService } from './OpsService';
-
+import { v4 as uuidv4 } from 'uuid';
+const wafPostfix = '-waf';
+const IPRuleSetPostFix = '-ip-set';
 export class WafService {
   public static async create(input: IExecuteInput) {
     const opsSvc = await OpsService.create(input);
@@ -41,9 +43,136 @@ export class WafService {
     });
   }
 
-  private async getRegionOfDeployment(deploymentName: string) {
+  private async getRegionOfDeployment(deploymentName: string): Promise<string | undefined> {
     const dynamoSdk = await this.getDynamoSDK({
       region: this.config.region,
     });
+    const deployments = await dynamoSdk.scan({ TableName: 'ops.deployment' }).promise();
+    if ((deployments.Count as number) === 0) {
+      return undefined;
+    }
+    const item = deployments.Items?.filter((item) => item.DeploymentName.S === deploymentName);
+    if (!item || item.length === 0 || item.length > 1) {
+      return undefined;
+    }
+    return item[0].region.S;
+  }
+
+  private async getWafOrUndefined(deploymentName: string, region: string) {
+    const wafSdk = await this.getWafSdk({
+      region,
+    });
+    const wafs = await wafSdk.listWebACLs({ Scope: 'REGIONAL' }).promise();
+    const waf = wafs.WebACLs?.find((waf) => waf.Name === deploymentName + wafPostfix);
+    return waf;
+  }
+
+  private async addRuleGroup(deploymentName: string, ruleGroupArn: string) {}
+
+  private async getRuleGroupFromName(region: string, ruleGroupName: string) {
+    const wafSdk = await this.getWafSdk({
+      region,
+    });
+
+    const RuleGroups = await wafSdk.listRuleGroups({ Scope: 'REGIONAL' }).promise();
+    return RuleGroups.RuleGroups?.find((ruleGroup) => ruleGroup.Name === ruleGroupName);
+  }
+
+  private async blockIP(deploymentName: string, region: string, ip: string) {
+    const wafSdk = await this.getWafSdk({
+      region,
+    });
+    const IPSet = await this.getIPSetOrUndefined(deploymentName, region);
+    if (!IPSet) {
+      throw Error('Can not find the IPSet, please re run deployment add.');
+    }
+    const ipsetDetails = await wafSdk
+      .getIPSet({
+        Scope: 'REGIONAL',
+        Name: IPSet.Name as string,
+        Id: IPSet.Id as string,
+      })
+      .promise();
+
+    await wafSdk
+      .updateIPSet({
+        Scope: 'REGIONAL',
+        Name: ipsetDetails.IPSet?.Name as string,
+        Id: ipsetDetails.IPSet?.Id as string,
+        Addresses: [...(ipsetDetails.IPSet?.Addresses as WAFV2.IPAddresses), ip],
+        LockToken: ipsetDetails.LockToken as string,
+      })
+      .promise();
+  }
+  private async getWafJson(deploymentName: string, region: string) {
+    const waf = await this.getWafOrUndefined(deploymentName, region);
+    if (!waf) {
+      await this.input.io.write('{}');
+      return;
+    }
+    await this.input.io.write(
+      JSON.stringify({
+        name: waf.Name as string,
+        id: waf.Id as string,
+      })
+    );
+  }
+  private async getIPSetOrUndefined(deploymentName: string, region: string) {
+    const wafSdk = await this.getWafSdk({
+      region,
+    });
+    const ipsets = await wafSdk
+      .listIPSets({
+        Scope: 'REGIONAL',
+      })
+      .promise();
+
+    return ipsets.IPSets?.find((ipset) => ipset.Name === deploymentName + IPRuleSetPostFix);
+  }
+
+  private async updateRegexRuleSet(deploymentName: string, region: string, regexString: string) {
+    if (!this.validateRegex(regexString)) {
+      throw Error('Invalid RegEx string inputed.');
+    }
+    const waf = await this.getWafOrUndefined(deploymentName, region);
+    if (!waf) {
+      throw Error('Can not find WAF, please re-run fuse-ops deployment add to ensure WAF is enabled.');
+    }
+    const wafSdk = await this.getWafSdk({
+      region,
+    });
+    const wafDetails = await wafSdk
+      .getWebACL({ Scope: 'REGIONAL', Name: waf.Name as string, Id: waf.Id as string })
+      .promise();
+    const rules = wafDetails.WebACL?.Rules as WAFV2.Rules;
+    rules.push({
+      Name: uuidv4(),
+      Priority: 420,
+      Statement: {
+        RegexMatchStatement: {
+          RegexString: regexString,
+          FieldToMatch: { UriPath: {} },
+          TextTransformations: [{ Priority: 0, Type: 'None' }],
+        },
+      },
+      VisibilityConfig: { CloudWatchMetricsEnabled: false, SampledRequestsEnabled: false, MetricName: uuidv4() },
+    });
+    await wafSdk.updateWebACL({
+      Name: wafDetails.WebACL?.Name as string,
+      LockToken: wafDetails.LockToken as string,
+      DefaultAction: wafDetails.WebACL?.DefaultAction as WAFV2.DefaultAction,
+      Rules: wafDetails.WebACL?.Rules as WAFV2.Rules,
+      Scope: 'REGIONAL',
+      Id: wafDetails.WebACL?.Id as string,
+      VisibilityConfig: wafDetails.WebACL?.VisibilityConfig as WAFV2.VisibilityConfig,
+    });
+  }
+  private validateRegex(regexString: string) {
+    try {
+      new RegExp(regexString);
+      return true;
+    } catch (_) {
+      return false;
+    }
   }
 }
