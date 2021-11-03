8e918ea11506ad28220ad710f8d8992f31de6e9f
Matthew Zhao <matthew@fusebit.io>
2021-11-03 11:32:39 -0700

    .

--- tool/cicd/jenkins/scripts/test.sh
+++ tool/cicd/jenkins/scripts/test.sh
@@ -6,13 +6,8 @@ set -x
 # Run tests
 cd api/function-api
 
-# FUSE_PROFILE=cicd EC2=1 yarn test -i --no-colors --outputFile testOutput.json --json | true
+FUSE_PROFILE=cicd EC2=1 yarn test -i --no-colors --outputFile testOutput.json --json | true
 
 # Get logs
-<<<<<<< HEAD
 export JENKINS_STACK_ADD_TIME=$(cat /tmp/jenkins_start_time)
 ../../tool/cicd/jenkins/scripts/get_log_stream.js
-=======
-pwd
-./tool/cicd/jenkins/scripts/get_log_stream.js
->>>>>>> acb010d8 (.)
