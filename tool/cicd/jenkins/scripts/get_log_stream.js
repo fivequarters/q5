79a6179336a315c6daa681f214ca13ed3cb40c0e
Matthew Zhao <matthew@fusebit.io>
2021-11-02 21:41:13 -0700

    .

--- tool/cicd/jenkins/scripts/get_log_stream.js
+++ tool/cicd/jenkins/scripts/get_log_stream.js
@@ -3,21 +3,21 @@ let AWS = require('aws-sdk');
 const CWSdk = new AWS.CloudWatchLogs({
   region: 'us-east-2',
 });
-const startTime = process.env.JENKINS_STACK_ADD_TIME;
 
 (async () => {
   const logGroups = await CWSdk.describeLogStreams({
     logGroupName: '/fusebit-mono/jenkins',
   }).promise();
-  const correctLogStreams = logGroups.logStreams.filter((logGroup) => logGroup.firstEventTimestamp > startTime);
-  correctLogStreams.map((logStreamName) => {
+  const correctLogStreams = logGroups.logStreams.filter((logGroup) => logGroup.firstEventTimestamp > process.env.JENKINS_STACK_ADD_TIME);
+  console.log(correctLogStreams)
+  correctLogStreams.map(async (logStreamName) => {
     do {
       let nextToken;
       const results = await CWSdk.getLogEvents({
         logGroupName: '/fusebit-mono/jenkins',
-        logStreamName: logStreamName,
+        logStreamName: logStreamName.logStreamName,
       }).promise();
-      results.events.map((event) => {
+      results.events.forEach((event) => {
         console.log(event.message);
       });
       nextToken = results.nextForwardToken;
