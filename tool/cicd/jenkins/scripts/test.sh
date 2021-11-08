a3e76a4f7c5867cd961e4363c2efa9d4374d60c9
Matthew Zhao <matthew@fusebit.io>
2021-11-08 13:50:23 -0800

    ert "."
    This reverts commit 3e0c7a9e04fdb9ecbbcb6fefee659175d51d9a58.

--- tool/cicd/jenkins/scripts/test.sh
+++ tool/cicd/jenkins/scripts/test.sh
@@ -6,7 +6,7 @@ set -x
 # Run tests
 cd api/function-api
 
-FUSE_PROFILE=cicd EC2=1 yarn test -i --no-colors --outputFile testOutput.json --json | true
+# FUSE_PROFILE=cicd EC2=1 yarn test -i --no-colors --outputFile testOutput.json --json | true
 
 # Get logs
 export JENKINS_STACK_ADD_TIME=$(cat /tmp/jenkins_start_time)
