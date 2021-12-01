f69c8543f371e78897ad32885cd0062de2fcc9b0
Matthew Zhao <matthew@fusebit.io>
2021-12-01 15:11:15 -0800

    file name

--- tool/cicd/actions/scripts/update_s3.mjs
+++ tool/cicd/actions/scripts/update_s3.mjs
@@ -11,6 +11,6 @@ const keys = [
   keys.forEach(async (key) => {
     await $`wget https://cdn.segment.com/analytics.js/v1/${key}/analytics.min.js`;
     await $`mv analytics.min.js ${key}.js`;
-    await $`aws s3 cp --acl public-read --cache-control max-age=300 analytics.min.js s3://fusebit-io-cdn/data/js/`;
+    await $`aws s3 cp --acl public-read --cache-control max-age=300 ${key}.js s3://fusebit-io-cdn/data/js/`;
   });
 })();
