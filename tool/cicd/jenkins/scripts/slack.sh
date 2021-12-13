23ba6de4fdacf49a486f0957b163ad3859365930
Benn Bollay <bb.github@magitech.org>
2021-12-12 21:03:59 -0800

    Some slight improvements to the output of the slack message

--- tool/cicd/jenkins/scripts/slack.sh
+++ tool/cicd/jenkins/scripts/slack.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-git log -n 3  --pretty=%aN > commit.txt
+git log -n 10  --pretty=%aN,%h,%s > commit.txt
 node tool/cicd/slack-notification/libc/index.js
--- tool/cicd/slack-notification/src/index.ts
+++ tool/cicd/slack-notification/src/index.ts
@@ -1,93 +1,200 @@
 #!/usr/bin/env node
 
+import fs from 'fs';
 import superagent from 'superagent';
+
 import { JestOutput, Status } from './jestTypes';
-import { promises as fs } from 'fs';
-const outputJson: JestOutput = require('../../../../api/function-api/testOutput.json');
+
+interface ICommitEntry {
+  name: string;
+  hash: string;
+  message: string;
+}
 
 const failureWebhook = process.env.FAILURE_WEBHOOK as string;
 const successWebhook = process.env.SUCCESS_WEBHOOK as string;
 
-const nameToMention = [
-  { name: 'Matthew Zhao', id: '<@U01UDTF3VQR>' },
-  { name: 'Benn Bollay', id: '<@UUPT2SQN7>' },
-  { name: 'Ruben Restrepo', id: '<@U0277EMBRSN>' },
-  { name: 'Bruno Krebs', id: '<@U027X5JG8QG>' },
-  { name: 'Tomasz Janczuk', id: '<@UFN96HN1J>' },
-  { name: 'Yavor Georgiev', id: '<@UDGRLGJTG>' },
-  { name: 'Chris More', id: '<@U01NQDVLYKB>' },
-  { name: 'Shehzad Akbar', id: '<@U02CP37DEU8>' },
-  { name: 'Jacob Haller-Roby', id: '<@U01NQDVRW0Z>' },
-  { name: 'Liz Parody', id: '<@U02EJPA1MCJ>' },
-];
+const testPathPrefix = '/var/lib/jenkins/workspace/fusebit-test-suite/api/function-api/test/';
+const commitTextFile = '/var/lib/jenkins/workspace/fusebit-test-suite/commit.txt';
+const repositoryCommitUrl = 'https://github.com/fivequarters/q5/commit/';
+const jestOutput: JestOutput = JSON.parse(fs.readFileSync('../../../../api/function-api/testOutput.json', 'utf8'));
 
-(async () => {
-  if (outputJson.success) {
-    await superagent.post(successWebhook).send({
-      text: ':tada: Fusebit Test Suite Passed :tada:',
-    });
-    return;
-  }
-  const failurePayload = {
-    text: ':warning: Tests are failing :warning:',
-    blocks: [
+const nameToMention: Record<string, string> = {
+  'Matthew Zhao': '<@U01UDTF3VQR>',
+  'Benn Bollay': '<@UUPT2SQN7>',
+  'Ruben Restrepo': '<@U0277EMBRSN>',
+  'Bruno Krebs': '<@U027X5JG8QG>',
+  'Tomasz Janczuk': '<@UFN96HN1J>',
+  'Yavor Georgiev': '<@UDGRLGJTG>',
+  'Chris More': '<@U01NQDVLYKB>',
+  'Shehzad Akbar': '<@U02CP37DEU8>',
+  'Jacob Haller-Roby': '<@U01NQDVRW0Z>',
+  'Liz Parody': '<@U02EJPA1MCJ>',
+};
+
+const makeJenkinsUrl = (): string => `${process.env.BUILD_URL}/consoleText`;
+const makeTestbotUrl = (testBotId: string) =>
+  `https://testbot.dev.fusebit.io/ui/#q5-fusebit/launches/all/${testBotId}?item0Params=page.sort%3Dstatistics%2524executions%2524failed%252CDESC`;
+
+const parseCommits = (): ICommitEntry[] => {
+  const commitFile = fs.readFileSync(commitTextFile, 'utf8');
+  const commits = commitFile.split('\n');
+  return commits.map((commit) => {
+    const commitSplit = commit.split(',');
+    return {
+      name: commitSplit[0],
+      hash: commitSplit[1],
+      message: commitSplit.slice(2).join(','),
+    };
+  });
+};
+
+const slackBlockHeader = {
+  blocks: [
+    {
+      type: 'divider',
+    },
+    {
+      type: 'header',
+      text: {
+        type: 'plain_text',
+        text: 'Function API Test Failure',
+        emoji: true,
+      },
+    },
+  ],
+};
+
+const addErrorBlock = (block: any, failedTests: string[]) => {
+  const section = {
+    type: 'section',
+    fields: [
       {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: `:warning: :warning: :warning: ${outputJson.numFailedTestSuites} test suites have failed. :warning: :warning: :warning:`,
-        },
+        type: 'mrkdwn',
+        text: `*Failed: ${failedTests.length} tests*`,
+      },
+      {
+        type: 'mrkdwn',
+        text: failedTests[0],
       },
     ],
   };
-  for (const test of outputJson.testResults) {
-    if (test.status !== Status.Failed) {
-      continue;
-    }
 
-    failurePayload.blocks.push({
+  failedTests.slice(1).forEach((failedTest: string) => {
+    section.fields.push(
+      {
+        type: 'plain_text',
+        text: ' ',
+      },
+      {
+        type: 'mrkdwn',
+        text: failedTest,
+      }
+    );
+  });
+
+  block.blocks.push(section);
+};
+
+const addCommiterBlock = (block: any, commits: ICommitEntry[]) => {
+  const section = {
+    type: 'section',
+    fields: [
+      {
+        type: 'mrkdwn',
+        text: '*Last Committers*',
+      },
+      {
+        type: 'plain_text',
+        text: ' ',
+      },
+    ],
+  };
+
+  commits.forEach((commit) => {
+    section.fields.push(
+      {
+        type: 'mrkdwn',
+        text: `${nameToMention[commit.name] || commit.name}`,
+      },
+      {
+        type: 'mrkdwn',
+        text: `<${repositoryCommitUrl}${commit.hash}|${commit.hash}>: ${commit.message}`,
+      }
+    );
+  });
+  block.blocks.push(section);
+};
+
+const addFooter = (block: any, jenkinsUrl: string, testbotUrl: string) => {
+  block.blocks.push(
+    {
+      type: 'divider',
+    },
+    {
       type: 'section',
       text: {
-        type: 'plain_text',
-        text: `${test.name.split('/var/lib/jenkins/workspace/fusebit-test-suite/api/function-api/test/')[1]} Failed`,
+        type: 'mrkdwn',
+        text: ':gear: Open Testbot Report',
       },
-    });
-  }
-  failurePayload.blocks.push({
-    type: 'section',
-    text: {
-      type: 'mrkdwn',
-      text: `Check the logs here ${process.env.BUILD_URL}`,
+      accessory: {
+        type: 'button',
+        text: {
+          type: 'plain_text',
+          text: 'Open',
+          emoji: true,
+        },
+        value: 'click_me_123',
+        url: testbotUrl,
+        action_id: 'button-action',
+      },
     },
-  });
-
-  const runId = await fs.readFile('/tmp/rp-results-id');
-  failurePayload.blocks.push({
-    type: 'section',
-    text: {
-      type: 'mrkdwn',
-      text: `Check the testbot data here https://testbot.dev.fusebit.io/ui/#q5-fusebit/launches/all/${runId}`,
-    },
-  });
-
-  const commit = await fs.readFile('/var/lib/jenkins/workspace/fusebit-test-suite/commit.txt', 'utf8');
-  const commiters = commit.split('\n');
-  for (const commiter of commiters) {
-    for (const person of nameToMention) {
-      if (commiter.includes(person.name)) {
-        failurePayload.blocks.push({
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: `${person.id} is one of the last commiters.`,
-          },
-        });
-      }
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: ':scroll: View the Jenkins Logs',
+      },
+      accessory: {
+        type: 'button',
+        text: {
+          type: 'plain_text',
+          text: 'Open',
+          emoji: true,
+        },
+        value: 'click_me_123',
+        url: jenkinsUrl,
+        action_id: 'button-action',
+      },
     }
+  );
+};
+
+(async () => {
+  if (jestOutput.success) {
+    await superagent.post(successWebhook).send({
+      text: ':tada: Fusebit API Full Pass :tada:',
+    });
+    return;
   }
+
+  const failedTests = jestOutput.testResults
+    .filter((testResult) => testResult.status === Status.Failed)
+    .map((failedTest) => failedTest.name.replace(testPathPrefix, ''));
+  const commits = parseCommits();
+  const runId = fs.readFileSync('/tmp/rp-results-id', 'utf8');
+
+  const block = slackBlockHeader;
+
+  addErrorBlock(block, failedTests);
+  addCommiterBlock(block, commits);
+  addFooter(block, makeJenkinsUrl(), makeTestbotUrl(runId));
+
+  // Just in case it fails to send...
+  console.log(JSON.stringify(block, null, 2));
 
   try {
-    await superagent.post(failureWebhook).send(failurePayload);
+    await superagent.post(failureWebhook).send(block);
   } catch (e) {
     console.log(e);
   }
