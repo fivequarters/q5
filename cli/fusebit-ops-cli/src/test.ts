e133aa68198b45ab6d40c5bc750d5247e75f6df2
Matthew Zhao <matthew@fusebit.io>
2021-12-22 16:27:23 -0800

    .

---
+++ cli/fusebit-ops-cli/src/test.ts
@@ -0,0 +1,3 @@
+import * as GrafanaConfig from '@5qtrs/grafana-config';
+
+console.log(GrafanaConfig.getConfigTemplate());
---
+++ lib/grafana/config/src/index.ts
@@ -0,0 +1,12 @@
+import fs from 'fs';
+import path from 'path';
+import ini from 'ini';
+
+export const getConfigTemplate = () => {
+  const file = fs.readFileSync(path.join(__dirname, 'grafana.ini'), 'utf-8');
+  return ini.decode(file);
+};
+
+export const toIniFile = (input: string) => {
+  return ini.encode(input);
+};
