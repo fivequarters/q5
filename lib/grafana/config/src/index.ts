77036a02948e1229230a6de94d22c22b1ba5770e
Matthew Zhao <matthew@fusebit.io>
2022-02-10 19:55:32 -0800

    .

--- lib/grafana/config/src/index.ts
+++ lib/grafana/config/src/index.ts
@@ -4,12 +4,12 @@ import ini from 'ini';
 import yaml from 'js-yaml';
 
 export const getGrafanaConfigTemplate = () => {
-  const file = fs.readFileSync(path.join(__dirname, 'grafana.ini'), 'utf-8');
+  const file = fs.readFileSync(path.join(__dirname, 'config', 'grafana.ini'), 'utf-8');
   return ini.decode(file);
 };
 
 export const getDockerComposeTemplate = () => {
-  const file = fs.readFileSync(path.join(__dirname, 'docker-compose.yml'), 'utf-8');
+  const file = fs.readFileSync(path.join(__dirname, 'config', 'docker-compose.yml'), 'utf-8');
   return yaml.load(file);
 };
 
