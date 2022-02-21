6b759d560c92ad647bd6d1d55ce1117f00d8b9be
Matthew Zhao <matthew@fusebit.io>
2022-02-20 21:42:25 -0800

    .

--- _templates/proxy-service/new/prompt.js
+++ _templates/proxy-service/new/prompt.js
@@ -1,10 +1,8 @@
-// prettier-ignore
 const requireAllValues = (value) =>
   Object.entries(value).reduce((prev, entry) => prev || entry[1].length === 0, false)
     ? 'All fields must be supplied'
     : true;
 
-// prettier-ignore
 module.exports = [
   {
     type: 'input',
