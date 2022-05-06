16728a0539f526daf3498457ed13ec8c46167795
Benn Bollay <bb.github@magitech.org>
2022-05-06 12:10:35 -0700

    Soften restrictions on redirect domain names for function serve (#1396)

--- api/function-api/src/routes/validation/redirect.js
+++ api/function-api/src/routes/validation/redirect.js
@@ -1,7 +1,11 @@
 const Joi = require('joi');
 
-const REDIRECT_REGEX_MATCH = new RegExp('^https://[a-z0-9-]+\\.tunnel\\.dev\\.fusebit\\.io$');
+const REDIRECT_REGEX_MATCH = new RegExp('^https://[a-z0-9-]+\\.tunnel\\.dev\\.fivequarters\\.io$');
 
 module.exports = Joi.object().keys({
-  redirectUrl: Joi.string().uri({ scheme: 'https' }).regex(REDIRECT_REGEX_MATCH).required(),
+  redirectUrl: Joi.string()
+    .uri({ scheme: 'https' })
+    .regex(REDIRECT_REGEX_MATCH)
+    .required()
+    .error(() => 'Unsupported tunnel endpoint'),
 });
