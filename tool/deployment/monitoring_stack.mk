828d53b524772e7d73c64ea0b61acade53f9f61a
Benn Bollay <bb.github@magitech.org>
2022-09-23 11:54:28 -0700

    NFC: Update monitoring stack tooling to reflect new fuse-ops

--- tool/deployment/monitoring_stack.mk
+++ tool/deployment/monitoring_stack.mk
@@ -2,18 +2,20 @@ include tool/deployment/profile.mk
 
 .ONESHELL:
 
-DEFAULT_GRAFANA_VER:=`curl -s https://raw.githubusercontent.com/fusebit/grafana/fusebit-v1.0.0-v8.3.3/.fusebit.version.json | jq -rc ".version"`
+DEFAULT_LOKI_VER:=grafana/loki:2.5.0
+DEFAULT_TEMPO_VER:=grafana/tempo:1.5.0
+DEFAULT_GRAFANA_VER:=fusebit/grafana:`curl -s https://raw.githubusercontent.com/fusebit/grafana/fusebit-v1.0.0-v8.3.3/.fusebit.version.json | jq -rc ".version"`
 
 %.stack-mon-prod: 321.require-profile
-	fuse-ops monitoring stack add ${NAME} --grafanaTag ${DEFAULT_GRAFANA_VER} --region ${REGION}
+	fuse-ops monitoring stack add ${NAME} ${DEFAULT_LOKI_VER} ${DEFAULT_TEMPO_VER} ${DEFAULT_GRAFANA_VER} --region ${REGION}
 
 %.stack-mon-self: IMG_VER:=`jq -rc ".version" package.json`
 %.stack-mon-self: 763.require-profile
-	fuse-ops monitoring stack add ${NAME} --grafanaTag ${DEFAULT_GRAFANA_VER} --region ${REGION}
+	fuse-ops monitoring stack add ${NAME} ${DEFAULT_LOKI_VER} ${DEFAULT_TEMPO_VER} ${DEFAULT_GRAFANA_VER} --region ${REGION}
 
 %.stack-mon-dev: IMG_VER:=`jq -rc ".version" package.json`
 %.stack-mon-dev: 749.require-profile
-	fuse-ops monitoring stack add ${NAME} --grafanaTag ${DEFAULT_GRAFANA_VER} --region ${REGION}
+	fuse-ops monitoring stack add ${NAME} ${DEFAULT_LOKI_VER} ${DEFAULT_TEMPO_VER} ${DEFAULT_GRAFANA_VER} --region ${REGION}
 
 deploy-stack-monitoring-fuseprod-stage-us-west-2: stage-us-west-2.stack-mon-prod
 stage-us-west-2.stack-mon-prod: NAME=stage
