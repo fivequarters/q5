d228c141b5af706aace44c2675a21b0e1027e5cc
Matthew Zhao <matthewzhaocc@gmail.com>
2022-09-26 12:24:03 -0700

    Yet another commit

--- api/function-api/src/routes/grafana/health.ts
+++ api/function-api/src/routes/grafana/health.ts
@@ -3,15 +3,21 @@ import express from 'express';
 import create_error from 'http-errors';
 import * as Constants from '@5qtrs/constants';
 
+const GRAFANA_HEALTH_TIMEOUT = 10 * 1000;
+
 export const checkGrafanaHealth = () => {
   return async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const endpoint = Constants.GRAFANA_HEALTH_ENDPOINT;
     // Our internal Grafana+services healthcheck daemon implements a /healthz endpoint
-    const result = await superagent.get(new URL('/healthz', endpoint).toString());
-    if (!result.ok) {
-      return next(create_error(500, result.error as Error));
+    try {
+      const result = await superagent.get(new URL('/healthz', endpoint).toString()).timeout(GRAFANA_HEALTH_TIMEOUT);
+      if (result.ok) {
+        return res.json({ status: 'ok' });
+      }
+    } catch (_) {
+      // There is no error worth capturing here.
     }
 
-    return res.json({ status: 'ok' });
+    return next(create_error(500));
   };
 };
