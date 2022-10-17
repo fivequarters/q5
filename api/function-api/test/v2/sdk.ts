00cf339af7e0892488e65b75efd5b468b4d6e1a9
Matthew Zhao <46915203+matthewzhaocc@users.noreply.github.com>
2022-10-17 08:27:28 -0700

    Update sdk.ts

--- api/function-api/test/v2/sdk.ts
+++ api/function-api/test/v2/sdk.ts
@@ -71,7 +71,7 @@ export const cleanupEntities = async (account: IAccount) => {
     }
   }
 
-  await new (Promise as any)((res) => setTimeout(5000, res));
+  await new (Promise as any)((res) => setTimeout(res, 5000));
 };
 
 export const v2Request = async (account: IAccount, options: IRequestOptions) => {
