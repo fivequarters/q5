75b951c41dd772ee180f0b49ead0233fc9092f21
Matthew Zhao <matthewzhaocc@gmail.com>
2022-11-08 12:23:43 -0800

    .

--- tool/cicd/actions/scripts/deploy_exclusive_function_api.sh
+++ tool/cicd/actions/scripts/deploy_exclusive_function_api.sh
@@ -34,7 +34,7 @@ fi
 
 # -- Optional Parameters --
 # Pin to a specific version
-IMG_VER=${VERSION_FUNCTION_API:=$(jq -r '.version' ./package.json)}
+IMG_VER=${VERSION_FUNCTION_API:=`jq -r '.version' ./package.json`}
 
 # Is there an environment file present?
 if [ -z ${ENV_FILE+x} ]; then
@@ -47,13 +47,13 @@ fi
 
 # -- Script --
 
-ALL_STACKS=$(${FUSEOPS} stack ls -o json --deployment ${DEPLOYMENT_NAME})
-OLD_STACKS=$(echo ${ALL_STACKS} | jq --arg region ${REGION} -r 'map(select(.region == $region)) | .[] | .id')
+ALL_STACKS=`${FUSEOPS} stack ls -o json --deployment ${DEPLOYMENT_NAME}`
+OLD_STACKS=`echo ${ALL_STACKS} | jq --arg region ${REGION} -r 'map(select(.region == $region)) | .[] | .id'`
 
 echoerr "Deleting old stacks: ${OLD_STACKS}"
-echo -n ${OLD_STACKS} |
+echo -n ${OLD_STACKS} | \
   xargs -d ' ' -I STACKID \
-    ${FUSEOPS} stack rm ${DEPLOYMENT_NAME} STACKID --force true -c f --region ${REGION} -o json 1>&2
+  ${FUSEOPS} stack rm ${DEPLOYMENT_NAME} STACKID --force true -c f --region ${REGION} -o json 1>&2
 
 FUSEBIT_DEBUG=1 ${FUSEOPS} setup -c false
 FUSEBIT_DEBUG=1 ${FUSEOPS} deployment add ${DEPLOYMENT_NAME} ${NETWORK_NAME} ${DEPLOYMENT_DOMAIN} --dataWarehouse false --size 1 --region ${REGION} -c false
@@ -61,8 +61,8 @@ FUSEBIT_DEBUG=1 ${FUSEOPS} deployment add ${DEPLOYMENT_NAME} ${NETWORK_NAME} ${D
 echoerr "Deploying stack ${DEPLOYMENT_NAME}/${REGION}: ${IMG_VER} with environment params: ${ENV_PARAMS}"
 
 STACK_ADD_PARAMS="--region ${REGION} -c false --instance-size t3a.xlarge --size 1 -o json ${ENV_PARAMS}"
-STACK_ADD=$(${FUSEOPS} stack add ${DEPLOYMENT_NAME} ${IMG_VER} ${STACK_ADD_PARAMS})
-NEW_STACK_ID=$(echo ${STACK_ADD} | jq -r '.id')
+STACK_ADD=`${FUSEOPS} stack add ${DEPLOYMENT_NAME} ${IMG_VER} ${STACK_ADD_PARAMS}`
+NEW_STACK_ID=`echo ${STACK_ADD} | jq -r '.id'`
 
 echoerr "Promoting stack ${DEPLOYMENT_NAME}: ${NEW_STACK_ID}"
 ${FUSEOPS} stack promote ${DEPLOYMENT_NAME} ${NEW_STACK_ID} --region ${REGION} -c f 1>&2
